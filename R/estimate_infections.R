6471d45edbf6f26d585b4652f3125b9ad3db3a58
seabbs <s.e.abbott12@gmail.comm>
2020-06-17 15:49:49 +0000

    add new R and infection fn

---
+++ R/estimate_infections.R
@@ -0,0 +1,322 @@
+#' Estimate Infections, the Time-Varying Reproduction Number and the Rate of Growth
+#'
+#' @description This function uses a non-parametric approach to reconstruct cases by date of infection from reported 
+#' cases. It can optionally then estimate the time-varying reproduction number and the rate of growth.
+#' @export
+#' @importFrom rstan sampling extract 
+#' @importFrom data.table data.table copy merge.data.table as.data.table setorder rbindlist setDTthreads melt .N
+#' @importFrom purrr tranpose map_dbl
+#' @importFrom lubridate wday
+#' @importFrom truncnorm rtruncnorm
+#' @examples
+#' reported_cases <- NCoVUtils::get_ecdc_cases(countries = "Russia")
+#' reported_cases <- NCoVUtils::format_ecdc_data(reported_cases)
+#' reported_cases <- data.table::as.data.table(reported_cases)[, confirm := cases][, cases := NULL]
+#'  
+#' generation_time <- list(mean = EpiNow2::covid_generation_times_summary[1, ]$mean,
+#'                         mean_sd = EpiNow2::covid_generation_times_summary[1, ]$mean_sd,
+#'                         sd = EpiNow2::covid_generation_times_summary[1, ]$sd,
+#'                         sd_sd = EpiNow2::covid_generation_times_summary[1, ]$sd_sd,
+#'                         max = 30)
+#'                           
+#' incubation_period <- list(mean = EpiNow2::covid_incubation_period[1, ]$mean,
+#'                           mean_sd = EpiNow2::covid_incubation_period[1, ]$mean_sd,
+#'                           sd = EpiNow2::covid_incubation_period[1, ]$sd,
+#'                           sd_sd = EpiNow2::covid_incubation_period[1, ]$sd_sd,
+#'                           max = 30)
+#'                    
+#' reporting_delay <- list(mean = log(5),
+#'                         mean_sd = log(2),
+#'                         sd = log(2),
+#'                         sd_sd = log(1.5),
+#'                         max = 30)
+#'                         
+#' rt_prior <- list(mean = 2.6, sd = 2)
+#'   
+#' ## Compile model
+#' model <- rstan::stan_model("inst/estimate_infections.stan")
+#' 
+#' ## Run model
+#' out <- estimate_infections(reported_cases, family = "poisson",
+#'                            generation_time = generation_time,
+#'                            incubation_period = incubation_period,
+#'                            reporting_delay = reporting_delay,
+#'                            rt_prior = rt_prior,
+#'                            model = model,
+#'                            cores = 4, chains = 4,
+#'                            estimate_rt = TRUE,
+#'                            verbose = TRUE, return_fit = TRUE)
+#'
+#' out                                   
+estimate_infections <- function(reported_cases, family = "negbin",
+                                incubation_period, reporting_delay,
+                                generation_time, rt_prior,
+                                prior_smoothing_window = 7,
+                                model, cores = 1, chains = 2,
+                                samples = 1000, warmup = 1000,
+                                estimate_rt = FALSE, adapt_delta = 0.95,
+                                max_treedepth = 15, return_fit = FALSE,
+                                verbose = FALSE){
+  
+  suppressMessages(data.table::setDTthreads(threads = 1))
+  
+  # Add prior for R if missing ---------------------------------
+  
+  if (missing(rt_prior)) {
+    rt_prior <- list(mean = 1, sd = 1)
+  }
+  
+  # Make sure there are no missing dates and order cases --------------------
+  reported_cases_grid <- data.table::copy(reported_cases)[, .(date = seq(min(date), max(date), by = "days"))]
+  
+  nowcast <- data.table::merge.data.table(
+    reported_cases , reported_cases_grid, 
+    by = c("date"), all.y = TRUE)
+  
+  reported_cases <- reported_cases[is.na(confirm), confirm := 0 ][,.(date = date, confirm)]
+  reported_cases <- data.table::setorder(reported_cases, date)
+  
+  ## Filter out 0 reported cases from the end of the data
+  reported_cases <- reported_cases[order(-date)][, 
+                                 cum_cases := cumsum(confirm)][cum_cases > 0][, cum_cases := NULL]
+  ## Filter out 0 reported cases from the beginning of the data
+  reported_cases <- reported_cases[order(date)][,
+                                 cum_cases := cumsum(confirm)][cum_cases > 0][, cum_cases := NULL]
+  
+  # Estimate the mean delay -----------------------------------------------
+  
+  mean_shift <- as.integer(exp(incubation_period$mean) + exp(reporting_delay$mean))
+  
+  # Add the mean delay and incubation period on as 0 case days ------------
+  
+  reported_cases <- data.table::rbindlist(list(
+    data.table::data.table(date = seq(min(reported_cases$date) - mean_shift - prior_smoothing_window, 
+                                      min(reported_cases$date) - 1, by = "days"),
+                           confirm = 0),
+    reported_cases
+  ))  
+  
+  # Calculate smoothed prior cases ------------------------------------------
+  
+  shifted_reported_cases <- data.table::copy(reported_cases)[,
+                    confirm := data.table::shift(confirm, n = mean_shift,
+                                                 type = "lead", 
+                                                 fill = mean(data.table::last(confirm, n = 7)))][,
+                                                                                                                                                         confirm := data.table::frollmean(confirm, n = prior_smoothing_window, 
+                                                                                                                                                                                          align = "right", fill = data.table::last(confirm))][,
+                                                                                                                                                                                                                                              confirm := data.table::fifelse(confirm == 0, 1e-4, confirm)]
+  
+  ##Drop median generation interval initial values
+  shifted_reported_cases <- shifted_reported_cases[-(1:prior_smoothing_window)]
+  reported_cases <- reported_cases[-(1:prior_smoothing_window)]
+  
+  # Add week day info -------------------------------------------------------
+  
+  reported_cases <- reported_cases[, day_of_week := lubridate::wday(date, week_start = 1)][,
+                                     `:=`(wkd = ifelse(day_of_week >= 6, 1, 0),
+                                          mon = ifelse(day_of_week == 1, 1, 0))]
+  # Define stan model parameters --------------------------------------------
+  
+  data <- list(
+    day_of_week = reported_cases$day_of_week,
+    cases = reported_cases$confirm,
+    shifted_cases = shifted_reported_cases$confirm,
+    t = length(reported_cases$date),
+    rt = length(reported_cases$date) - mean_shift,
+    inc_mean_mean = incubation_period$mean,
+    inc_mean_sd = incubation_period$mean_sd,
+    inc_sd_mean = incubation_period$sd,
+    inc_sd_sd = incubation_period$sd_sd,
+    max_inc = incubation_period$max,
+    rep_mean_mean = reporting_delay$mean,
+    rep_mean_sd = reporting_delay$mean_sd,
+    rep_sd_mean = reporting_delay$sd,
+    rep_sd_sd = reporting_delay$sd_sd,
+    max_rep = reporting_delay$max,
+    gt_mean_mean = generation_time$mean,
+    gt_mean_sd = generation_time$mean_sd,
+    gt_sd_mean = generation_time$sd,
+    gt_sd_sd = generation_time$sd_sd,
+    max_gt = generation_time$max,
+    r_mean = rt_prior$mean,
+    r_sd = rt_prior$sd,
+    estimate_r = ifelse(estimate_rt, 1, 0)
+  )  
+  
+  ## Set model to poisson or negative binomial
+  if (family %in% "poisson") {
+    data$model_type <- 0
+  }else if (family %in% "negbin"){
+    data$model_type <- 1
+  }
+  
+  # Set up initial conditions fn --------------------------------------------
+  
+  init_fun <- function(){out <- list(
+    eta = rnorm(data$t, mean = 0, sd = 1),
+    rho = rlnorm(1, 1.609438, 0.5),
+    alpha =  truncnorm::rtruncnorm(1, a = 0, mean = 0, sd = 1),
+    inc_mean = truncnorm::rtruncnorm(1, a = 0, mean = incubation_period$mean, sd = incubation_period$mean_sd),
+    inc_sd = truncnorm::rtruncnorm(1, a = 0, mean = incubation_period$sd, sd = incubation_period$sd_sd),
+    rep_mean = truncnorm::rtruncnorm(1, a = 0, mean = reporting_delay$mean, sd = reporting_delay$mean_sd),
+    rep_sd = truncnorm::rtruncnorm(1, a = 0, mean = reporting_delay$sd,  sd = reporting_delay$sd_sd))
+  
+  if (data$model_type == 1) {
+    out$rep_phi <- array(rexp(1, 1))
+  }
+  
+  if (estimate_rt) {
+    out$R <- array(rgamma(n = 1, shape = (rt_prior$mean / rt_prior$sd)^2, 
+                          scale = (rt_prior$sd^2) / rt_prior$mean))
+    out$R_eta <- rnorm(data$rt, mean = 0, sd = 1)
+    out$R_rho <- array(rlnorm(1, 1.609438, 0.5))
+    out$R_alpha <-  array(truncnorm::rtruncnorm(1, a = 0, mean = 0, sd = 1))
+    out$gt_mean <- array(truncnorm::rtruncnorm(1, a = 1, mean = generation_time$mean,  
+                                               sd = generation_time$mean_sd))
+    out$gt_sd <-  array(truncnorm::rtruncnorm(1, a = 0, mean = generation_time$sd,
+                                              sd = generation_time$sd_sd))
+    
+    if (data$model_type == 1) {
+      out$inf_phi <- array(rexp(1, 1))
+    }
+  }
+  
+  return(out)
+  }
+  
+  # Load and run the stan model ---------------------------------------------
+  
+  if (missing(model)) {
+    model <- stanmodels$estimate_infections
+  }
+  
+  if (verbose) {
+    message(paste0("Running for ", samples + warmup," samples and ", data$t," time steps"))
+  }
+  
+  fit <-
+    rstan::sampling(model,
+                    data = data,
+                    chains = chains,
+                    init = init_fun,
+                    iter = samples + warmup, 
+                    warmup = warmup,
+                    cores = cores,
+                    control = list(adapt_delta = adapt_delta,
+                                   max_treedepth = max_treedepth),
+                    refresh = ifelse(verbose, 50, 0))
+  
+  # Extract parameters of interest from the fit -----------------------------
+  
+  ## Extract sample from stan object
+  samples <- rstan::extract(fit)
+  
+  ## Construct reporting list
+  out <- list()
+  
+  ## Generic data.frame reporting function
+  extract_parameter <- function(param, samples, dates) {
+    param_df <- data.table::as.data.table(
+      t(
+        data.table::as.data.table(
+          samples[[param]]
+        )
+      ))
+    
+    param_df <- param_df[, time := 1:.N]
+    param_df <- 
+      data.table::melt(param_df, id.vars = "time",
+                       variable.name = "var")
+    
+    param_df <- param_df[, var := NULL][, sample := 1:.N, by = .(time)]
+    param_df <- param_df[, date := dates, by = .(sample)]
+    param_df <- param_df[, .(parameter = param, time, date, 
+                             sample, value)]
+    
+    return(param_df)
+  }
+  
+  ## Report infections, and R
+  out$infections <- extract_parameter("imputed_infections", 
+                                      samples,
+                                      reported_cases$date)
+  
+  if (estimate_rt) {
+    out$R <- extract_parameter("R", 
+                               samples,
+                               reported_cases$date[-(1:mean_shift)])
+    
+    out$growth_rate <- extract_parameter("r", 
+                                         samples,
+                                         reported_cases$date[-(1:mean_shift)])
+  }
+  
+    ## Add prior infections
+    out$prior_infections <- shifted_reported_cases[, .(parameter = "prior_infections", time = 1:.N, 
+                                                       date, value = confirm, sample = 1)]
+    
+    out$noise <- extract_parameter("noise", 
+                                   samples,
+                                   reported_cases$date)
+    
+    out$day_of_week <- extract_parameter("day_of_week_eff", 
+                                         samples,
+                                         1:7)
+    
+    char_day_of_week <- data.table::data.table(wday = c("Monday", "Tuesday", "Wednesday",
+                                                        "Thursday", "Friday", "Saturday",
+                                                        "Sunday"),
+                                               time = 1:7)
+    out$day_of_week <- out$day_of_week[char_day_of_week, on = "time"][, 
+                                       wday_numeric := time][,time := NULL]
+    
+    extract_static_parameter <- function(param) {
+      data.table::data.table(
+        parameter = param,
+        sample = 1:length(samples[[param]]),
+        value = samples[[param]])
+    }
+    
+    out$inc_mean <- extract_static_parameter("inc_mean")
+    
+    out$inc_sd <- extract_static_parameter("inc_sd")
+    
+    out$rep_mean <- extract_static_parameter("rep_mean")
+    
+    out$rep_sd <- extract_static_parameter("rep_sd")
+    
+    if (estimate_rt) {
+      out$gt_mean <- extract_static_parameter("gt_mean")
+      out$gt_mean[, value := value.V1][, value.V1 := NULL]
+      
+      out$gt_sd <- extract_static_parameter("gt_sd")
+      out$gt_sd[, value := value.V1][, value.V1 := NULL]
+    }
+  
+
+# Format output -----------------------------------------------------------
+    
+ format_out <- list()
+  
+ ## Bind all samples together
+ format_out$samples <- data.table::rbindlist(out, fill = TRUE, idcol = "variable")
+ 
+ ## Summarise samples
+ format_out$samples <- data.table::copy(format_out$samples)[, .(
+   bottom  = as.numeric(purrr::map_dbl(list(HDInterval::hdi(cases, credMass = 0.9)), ~ .[[1]])),
+   top = as.numeric(purrr::map_dbl(list(HDInterval::hdi(cases, credMass = 0.9)), ~ .[[2]])),
+   lower  = as.numeric(purrr::map_dbl(list(HDInterval::hdi(cases, credMass = 0.5)), ~ .[[1]])),
+   upper = as.numeric(purrr::map_dbl(list(HDInterval::hdi(cases, credMass = 0.5)), ~ .[[2]])),
+   median = as.numeric(median(cases, na.rm = TRUE)),
+   mean = as.numeric(mean(cases, na.rm = TRUE))
+ ), by = .(date, variable)]
+ 
+ ## Order summarised samples
+ data.table::setorder(format_out$samples, variable, date)  
+ 
+ if (return_fit) {
+    format_out$fit <- fit
+ }
+ 
+  return(format_out)
+}
---
+++ inst/stan/estimate_infections.stan
@@ -0,0 +1,323 @@
+functions {
+  // convolve a pdf and case vector using matrix multiplication
+  vector convolve(vector cases, vector pdf, int direction) {
+    int t = num_elements(cases);
+    matrix[t, t] delay_mat = rep_matrix(0, t, t);
+    int max_pdf = num_elements(pdf);
+    row_vector[max_pdf] row_pdf = to_row_vector(pdf);
+    vector[t] convolved_cases;
+
+    for (s in 1:t) {
+      if (direction) {
+        int max_length = min(s, max_pdf);
+        delay_mat[s, (s - max_length + 1):s] = row_pdf[(max_pdf - max_length + 1):max_pdf];
+      }else{
+        int max_length = min(t - s, max_pdf - 1);
+        delay_mat[s, s:(s + max_length)] = row_pdf[1:(max_length + 1)];
+      }
+    }
+    
+   convolved_cases = delay_mat * to_vector(cases);
+
+   return(convolved_cases);
+  }
+
+  // apply backsampling and upscaling based on a pdf
+  vector backsample(vector cases, vector pdf) {
+    int t = num_elements(cases);
+    vector[t] backsampled_cases;
+    int max_upscale = min(t, num_elements(pdf));
+    int pdf_length = num_elements(pdf);
+    vector[pdf_length] cdf;
+    
+    backsampled_cases = convolve(cases, pdf, 0);
+    
+    // apply upscaling
+    cdf = cumulative_sum(pdf);
+    
+    for (i in  1:max_upscale) {
+      backsampled_cases[(t - i + 1)] = (backsampled_cases[(t - i + 1)] + 1) / cdf[i];
+    }
+    
+    //bound last day to equal day before
+    backsampled_cases[t] = backsampled_cases[t -1];
+    
+    return(backsampled_cases);
+  }
+  
+  // discretised truncated lognormal pmf
+  real discretised_lognormal_pmf(int y, real mu, real sigma, int max_val) {
+    return((normal_cdf((log(y + 1) - mu) / sigma, 0.0, 1.0) - normal_cdf((log(y) - mu) / sigma, 0.0, 1.0)) / 
+            normal_cdf((log(max_val) - mu) / sigma, 0.0, 1.0));
+  }
+  
+  // discretised truncated gamma pmf
+  real discretised_gamma_pmf(int y, real mu, real sigma, int max_val) {
+    // calculate alpha and beta for gamma distribution
+    real alpha = ((mu)/ sigma)^2;
+    real beta = (mu) / (sigma^2);
+    return((gamma_cdf(y, alpha, beta) - gamma_cdf(y - 1, alpha, beta)) / gamma_cdf(max_val, alpha, beta));
+  }
+}
+
+
+data {
+  int t;                             // number of time steps
+  int rt;                            // time over which to estimate rt
+  int day_of_week[t];                // day of the week indicator (1 - 7)
+  int <lower = 0> cases[t];          // observed cases
+  vector<lower = 0>[t] shifted_cases;// median shifted smoothed cases
+  real inc_mean_sd;                  // prior sd of mean incubation period
+  real inc_mean_mean;                // prior mean of mean incubation period
+  real inc_sd_mean;                  // prior sd of sd of incubation period
+  real inc_sd_sd;                    // prior sd of sd of incubation period
+  int max_inc;                       // maximum incubation period
+  real rep_mean_mean;                // prior mean of mean reporting delay
+  real rep_mean_sd;                  // prior sd of mean reporting delay
+  real rep_sd_mean;                  // prior mean of sd of reporting delay
+  real rep_sd_sd;                    // prior sd of sd of reporting delay
+  int max_rep;                       // maximum report delay
+  real <lower = 0> r_mean;           // prior mean of reproduction number
+  real <lower = 0> r_sd;             // prior standard deviation of reproduction number
+  real gt_mean_sd;                   // prior sd of mean generation time
+  real gt_mean_mean;                 // prior mean of mean generation time
+  real gt_sd_mean;                   // prior sd of sd of generation time
+  real gt_sd_sd;                     // prior sd of sd of generation time
+  int max_gt;                        // maximum generation time
+  int model_type;                    // type of model: 0 = poisson otherwise negative binomial
+  int estimate_r;                    // should the reproduction no be estimated (1 = yes)
+}
+
+transformed data{
+  real r_alpha;                      // alpha parameter of the R gamma prior
+  real r_beta;                       // beta parameter of the R gamma prior
+  real delta;                        // modifier to make GP + definite
+  real time[t];                      // time vector
+  int no_rt_time;                    // time without estimating Rt
+  
+  // calculate alpha and beta for gamma distribution
+  r_alpha = (r_mean / r_sd)^2;
+  r_beta = r_mean / (r_sd^2);
+  
+  // assign + definite term
+   delta = 1e-9;
+  
+  // make time vector
+   for (s in 1:t) {
+     time[s] = s;
+   }
+   
+   // time without estimating Rt is the differrence of t and rt
+   no_rt_time = t - rt;
+}
+parameters{
+  simplex[7] day_of_week_eff_raw;                     // day of week reporting effect + control parameters
+  real <lower = 0> inc_mean;                          // mean of incubation period
+  real <lower = 0> inc_sd;                            // sd of incubation period
+  real <lower = 0> rep_mean;                          // mean of reporting delay
+  real <lower = 0> rep_sd;                            // sd of incubation period
+  real<lower = 0> rep_phi[model_type];                // overdispersion of the reporting process
+  real<lower=0> rho;                                  // length scale of noise GP
+  real<lower=0> alpha;                                // scale of of noise GP
+  vector[t] eta;                                      // unconstrained noise
+  vector<lower = 0>[estimate_r] initial_R;            // baseline reproduction number estimate
+  real<lower = 1> gt_mean[estimate_r];                // mean of generation time
+  real <lower = 0> gt_sd[estimate_r];                 // sd of generation time
+  real<lower=0> inf_phi[model_type*estimate_r];       // overdispersion of the infection process
+  real<lower=0> R_rho[estimate_r];                    // length scale of R GP
+  real<lower=0> R_alpha[estimate_r];                  // scale of R GP
+  vector[estimate_r > 0 ? rt : 0] R_eta;              // unconstrained R noise
+}
+
+transformed parameters {
+  // stored transformed parameters
+  vector<lower = 0>[t] noise;                             // noise on the mean shifted observed cases
+  vector<lower = 0>[t] infections;                        // infections over time
+  vector<lower = 0>[t] reports;                           // reports over time
+  vector[7] day_of_week_eff;                              // day of the week effect
+  vector[estimate_r > 0 ? rt : 0] branch_reports;         // reports generated by the branching process
+  vector[estimate_r > 0 ? rt : 0] R;                      // reproduction number over time
+  
+ {
+  // temporary transformed parameters
+  vector[max_rep] rev_delay;                              // reversed report delay pdf
+  vector[max_inc] rev_incubation;                         // reversed incubation period pdf
+  vector[t] onsets;                                       // onsets over time
+  vector[estimate_r > 0 ? max_gt : 0] rev_generation_time;// reversed generation time pdf
+  vector[estimate_r > 0 ? t : 0] infectiousness;          // infections over time
+  vector[estimate_r > 0 ? t : 0] branch_onsets;           // onsets generated by the branching process
+  vector[estimate_r > 0 ? t : 0] branch_infections;       // infections generated by the branching process
+  matrix[t, t] K;                                         // covariance matrix 
+  matrix[t, t] L_K;                                       // cholesky docomposed covariance matrix
+  matrix[rt, rt] rK;                                      // covariance matrix for R estimation
+  matrix[rt, rt] rL_K;                                    // cholesky docomposed covariance matrix for R estimation
+  vector[estimate_r > 0 ? rt : 0] R_noise;                // noise on R centred on 1
+  
+  // reverse the distributions to allow vectorised access
+  for (j in 1:max_inc) {
+    rev_incubation[j] =
+        discretised_lognormal_pmf(max_inc - j + 1, inc_mean, inc_sd, max_inc);
+  }
+  
+  for (j in 1:(max_rep)) {
+    rev_delay[j] =
+        discretised_lognormal_pmf(max_rep - j + 1, rep_mean, rep_sd, max_rep);
+  }
+    
+  // define day of the week effect
+  day_of_week_eff = 7 * day_of_week_eff_raw;
+
+  // GP in noise
+  K = cov_exp_quad(time, alpha, rho);
+  // diagonal elements with offset to make + definite
+  for (n in 1:t) {
+      K[n, n] = K[n, n] + delta;
+  }
+  
+  L_K = cholesky_decompose(K);
+  noise = exp(L_K * eta);
+  
+  for (s in 1:t) {
+    if(noise[s] == 0) {
+      noise[s] = 0.0001;
+    }
+  }
+
+  // generate infections from prior infections and non-parameteric noise
+  infections = shifted_cases .* noise;
+  
+  // onsets from infections
+  onsets = convolve(infections, rev_incubation, 1);
+
+  // reports from onsets
+  reports = convolve(onsets, rev_delay, 1);
+
+  for (s in 1:t) {
+    // add reporting effects (adjust for simplex scale)
+    reports[s] *= day_of_week_eff[day_of_week[s]];
+  }
+    
+  ////////////////////////////////////////////////////// 
+  // estimate reproduction no from a branching process
+  //////////////////////////////////////////////////////
+  if (estimate_r) {
+    // calculate pdf of generation time from distribution
+    for (j in 1:(max_gt - 1)) {
+       rev_generation_time[j] =
+           discretised_gamma_pmf(max_gt - j, gt_mean[estimate_r], gt_sd[estimate_r], max_gt);
+     }
+     
+     // set same day to be 0
+     rev_generation_time[max_gt] = 0;
+     
+     infectiousness = convolve(infections, rev_generation_time, 1);
+
+     // Construct R over time
+      rK = cov_exp_quad(time[1:rt], R_alpha[estimate_r], R_rho[estimate_r]);
+     // diagonal elements with offset to make + definite
+      for (n in 1:rt) {
+        rK[n, n] = rK[n, n] + delta;
+        }
+      rL_K = cholesky_decompose(rK);
+      R_noise = exp(rL_K * R_eta);
+  
+     // Estimate infections using branching process
+     branch_infections[1:no_rt_time] = infections[1:no_rt_time];
+     
+      for (s in 1:rt) {
+         R[s] = initial_R[estimate_r] * R_noise[s];
+         branch_infections[s + no_rt_time] = R[s] * infectiousness[s + no_rt_time];
+         
+         // Make sure all dates have a non-zero value
+         if (branch_infections[s] == 0){
+            branch_infections[s] = 0.0001; 
+         }
+       }
+     // onsets from infections
+     branch_onsets = convolve(branch_infections, rev_incubation, 1);
+
+     // reports from onsets
+     {
+       vector[estimate_r > 0 ? t : 0] branch_reports_hold;
+       branch_reports_hold = convolve(branch_onsets, rev_delay, 1);
+       branch_reports = branch_reports_hold[(no_rt_time + 1):t];
+     }
+    
+     for (s in 1:rt) {
+      // add reporting effects (adjust for simplex scale)
+      branch_reports[s] *= (day_of_week_eff[day_of_week[s + no_rt_time]]);
+    }
+  }
+ }
+}
+
+model {
+  
+  // priors for noise GP
+  rho ~ lognormal(1.609438, 0.5); //log(5)
+  alpha ~ std_normal();
+  eta ~ std_normal();
+
+  // reporting overdispersion
+  if (model_type) {
+    rep_phi[model_type] ~ exponential(1);
+  }
+  
+  // daily cases given reports
+  if (model_type) {
+    target += neg_binomial_2_lpmf(cases | reports, rep_phi[model_type]);
+  }else{
+    target += poisson_lpmf(cases | reports);
+  }
+
+  // penalised priors for incubation period, and report delay
+  target += normal_lpdf(inc_mean | inc_mean_mean, inc_mean_sd) * t;
+  target += normal_lpdf(inc_sd | inc_sd_mean, inc_sd_sd) * t;
+  target += normal_lpdf(rep_mean | rep_mean_mean, rep_mean_sd) * t;
+  target += normal_lpdf(rep_sd | rep_sd_mean, rep_sd_sd) * t;
+  
+  ////////////////////////////////////////////////////// 
+  // estimate reproduction no from a branching process
+  //////////////////////////////////////////////////////
+  if (estimate_r) {
+    // infection overdispersion
+    if(model_type) {
+      inf_phi ~ exponential(1);
+    }
+
+  // prior on R
+  initial_R[estimate_r] ~ gamma(r_alpha, r_beta);
+    
+   // priors for R gp
+   R_rho ~ lognormal(1.609438, 0.5); //log(5)
+   R_alpha ~ std_normal();
+   R_eta ~ std_normal();
+    
+    // penalised_prior on generation interval
+    target += normal_lpdf(gt_mean | gt_mean_mean, gt_mean_sd) * rt;
+    target += normal_lpdf(gt_sd | gt_sd_mean, gt_sd_sd) * rt;
+    
+    // Likelihood of Rt given infections
+    if (model_type) {
+      target += neg_binomial_2_lpmf(cases[(no_rt_time + 1):t] | branch_reports, inf_phi[model_type*estimate_r]);
+    }else{
+      target += poisson_lpmf(cases[(no_rt_time + 1):t] | branch_reports);
+    }
+  }
+}
+  
+generated quantities {
+  int imputed_infections[t];
+  real r[estimate_r > 0 ? rt : 0];
+  
+  // simulated infections - assume poisson (with negative binomial reporting)
+  imputed_infections = poisson_rng(infections);
+  
+  // estimate the growth rate
+  if (estimate_r) {
+      real k = pow(gt_sd[estimate_r] / gt_mean[estimate_r], 2);
+      for (s in 1:rt) {
+        r[s] = (pow(R[s], k) - 1) / (k * gt_mean[estimate_r]);
+      } 
+  }
+}
