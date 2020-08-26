290e829b2600e4cec37a79b74a660a94dcd970c1
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-08-26 16:38:16 +0100

    changing null return

--- R/epinow.R
+++ R/epinow.R
@@ -408,7 +408,7 @@ regional_epinow <- function(reported_cases,
         ),
         TimeoutException = function(ex) {
           futile.logger::flog.warn("region %s timed out", target_region)
-          out <- ifelse (return_timings, list("timings" = Inf), NULL)
+          out <- ifelse (return_timings, list("timings" = Inf), list("estimates" = NULL))
         }
       )
     )
