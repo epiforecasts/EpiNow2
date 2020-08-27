8f8d27084aae6131202273c16acb3068443313e9
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-08-27 14:40:13 +0100

    perhaps a little more realistic

--- R/epinow.R
+++ R/epinow.R
@@ -343,7 +343,7 @@ regional_epinow <- function(reported_cases,
                             return_estimates = TRUE,
                             max_plot = 10,
                             return_timings = TRUE,
-                            max_execution_time = 1200, #todo: revert to Inf and timings to false
+                            max_execution_time = 1800, #todo: revert to Inf and timings to false
                             ...) {
 
   ## Set input to data.table
