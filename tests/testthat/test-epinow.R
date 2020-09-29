bf955d5f5686c422e8716c65ee72ecc0c6674a78
seabbs <s.e.abbott12@gmail.com>
2020-09-29 11:27:00 +0000

    speed up tests

--- tests/testthat/test-epinow.R
+++ tests/testthat/test-epinow.R
@@ -4,7 +4,7 @@ generation_time <- get_generation_time(disease = "SARS-CoV-2", source = "ganyani
 incubation_period <- get_incubation_period(disease = "SARS-CoV-2", source = "lauer", max_value = 15)
 reporting_delay <- EpiNow2::bootstrapped_dist_fit(rlnorm(100, log(3), 1), max_value = 15)
 
-reported_cases <- EpiNow2::example_confirmed[1:40]
+reported_cases <- EpiNow2::example_confirmed[1:30]
 
 df_non_zero <- function(df) {
   expect_true(nrow(df) > 0)
@@ -14,7 +14,7 @@ test_that("epinow produces expected output when run with default settings", {
   skip_on_cran()
   out <- suppressWarnings(epinow(reported_cases = reported_cases, generation_time = generation_time,
                 delays = list(incubation_period, reporting_delay),
-                samples = 200, stan_args = list(warmup = 100, cores = 1, chains = 2)))
+                samples = 100, stan_args = list(warmup = 100, cores = 1, chains = 2)))
   
   expect_equal(names(out), c("estimates", "estimated_reported_cases", "summary", "plots"))
   df_non_zero(out$estimates$samples)
@@ -31,7 +31,7 @@ test_that("epinow fails as expected when given a short timeout", {
   skip_on_cran()
   expect_error(suppressWarnings(epinow(reported_cases = reported_cases, generation_time = generation_time,
                 delays = list(incubation_period, reporting_delay),
-                samples = 500, stan_args = list(warmup = 100, cores = 1, chains = 2),
+                samples = 100, stan_args = list(warmup = 100, cores = 1, chains = 2),
                 max_execution_time = 10)))
 })
 
--- tests/testthat/test-estimate_infections.R
+++ tests/testthat/test-estimate_infections.R
@@ -5,14 +5,14 @@ reported_cases <- EpiNow2::example_confirmed[1:30]
 # Add a dummy breakpoint (used only when optionally estimating breakpoints)
 reported_cases <- reported_cases[, breakpoint := data.table::fifelse(date == as.Date("2020-03-16"),
                                                                      1, 0)]
-generation_time <- get_generation_time(disease = "SARS-CoV-2", source = "ganyani", max_value = 15)
-incubation_period <- get_incubation_period(disease = "SARS-CoV-2", source = "lauer", max_value = 15)
-reporting_delay <- list(mean = log(3), mean_sd = log(1.1), sd = log(2), sd_sd = log(1.1), max = 10)
+generation_time <- get_generation_time(disease = "SARS-CoV-2", source = "ganyani", max_value = 10)
+incubation_period <- get_incubation_period(disease = "SARS-CoV-2", source = "lauer", max_value = 10)
+reporting_delay <- list(mean = log(3), mean_sd = 0.1, sd = log(2), sd_sd = 0.1, max = 10)
 
 test_that("estimate_infections successfully returns estimates using default settings", {
   skip_on_cran()
   out <- suppressWarnings(estimate_infections(reported_cases, generation_time = generation_time,
-                             delays = list(reporting_delay), samples = 200, 
+                             delays = list(reporting_delay), samples = 100, 
                              stan_args=list(chains = 2, warmup = 100)))
   
   
@@ -21,15 +21,16 @@ test_that("estimate_infections successfully returns estimates using default sett
   expect_true(nrow(out$summarised) > 0)
 })
 
+
 test_that("estimate_infections fails as expected when given a very short timeout", {
   skip_on_cran()
   expect_error(estimate_infections(reported_cases, generation_time = generation_time,
                                    delays = list(reporting_delay),
-                                   samples = 500, stan_args=list(chains = 2, warmup = 100), 
+                                   samples = 100, stan_args=list(chains = 2, warmup = 100), 
                                    future = TRUE, max_execution_time = 10))
   expect_error(estimate_infections(reported_cases, generation_time = generation_time,
                                    delays = list(reporting_delay),
-                                   samples = 500, stan_args = list(chains = 2, warmup = 100),
+                                   samples = 100, stan_args = list(chains = 2, warmup = 100),
                                    future = FALSE, max_execution_time = 10))
 })
 
@@ -39,7 +40,7 @@ test_that("estimate_infections works as expected with failing chains", {
   skip_on_cran()
   out <- suppressWarnings(estimate_infections(reported_cases, generation_time = generation_time,
                                               delays = list(reporting_delay),
-                                              samples = 100, stan_args = list(chains = 4, warmup = 100,
+                                              samples = 100, stan_args = list(chains = 4, warmup = 50,
                                                                               stuck_chains = 2),
                                               future = TRUE))
   expect_equal(names(out), c("samples", "summarised"))
