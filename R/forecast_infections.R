32c6de05c0a9c9e326716301e9e485c1605115af
seabbs <s.e.abbott12@gmail.com>
2020-10-08 14:39:49 +0000

    review forecast_infections and update news

--- R/forecast_infections.R
+++ R/forecast_infections.R
@@ -38,6 +38,7 @@
 #' ## Estimate Rt and infections from data
 #' out <- estimate_infections(reported_cases, generation_time = generation_time,
 #'                            delays = list(incubation_period, reporting_delay),
+#'                            gp = list(),
 #'                            stan_args = list(cores = ifelse(interactive(), 4, 1), chains = 4))
 #'
 #' ## Forecast Rt and infections from estimates
