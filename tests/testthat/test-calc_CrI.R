7d4a32713bbe46a983f73d3c093e8eedd39d2ed1
seabbs <s.e.abbott12@gmail.com>
2020-10-09 14:34:16 +0000

    add tests for CrI fns

---
+++ tests/testthat/test-calc_CrI.R
@@ -0,0 +1,13 @@
+context("calc_CrI")
+
+
+samples <- data.frame(value = 1:10, type = "car")
+
+test_that("calc_CrI works as expected with default arguments", {
+  expect_known_output(calc_CrI(samples), file = "tests/test-data/calc_CrI_default.rds")
+})
+
+test_that("calc_CrI works as expected when grouping", {
+  expect_known_output(calc_CrI(samples, summarise_by = "type"),
+                      file = "tests/test-data/calc_CrI_grouping.rds")
+})
