f6bde1635fcedb40c32943a28815d6e4b2d876b1
seabbs <s.e.abbott12@gmail.com>
2020-10-09 14:34:16 +0000

    add tests for CrI fns

---
+++ tests/testthat/test-calc_CrIs.R
@@ -0,0 +1,19 @@
+context("calc_CrIs")
+
+
+samples <- data.frame(value = 1:10, type = "car")
+
+test_that("calc_CrI works as expected with default arguments", {
+  expect_known_output(calc_CrIs(samples), file = "tests/test-data/calc_CrIs_default.rds")
+})
+
+test_that("calc_CrI works as expected when grouping", {
+  expect_known_output(calc_CrIs(samples, summarise_by = "type"),
+                      file = "tests/test-data/calc_CrIs_grouping.rds")
+})
+
+
+test_that("calc_CrI works as expected when given a custom CrI list", {
+  expect_known_output(calc_CrIs(samples, CrIs = c(0.1, 0.4, 0.95)),
+                      file = "tests/test-data/calc_CrIs_custom_CrI.rds")
+})
---
+++ tests/testthat/test-calc_summary_measures.R
@@ -0,0 +1,19 @@
+context("calc_summary_measures")
+
+samples <- data.frame(value = 1:10, type = "car")
+
+test_that("calc_summary_measures works as expected with default arguments", {
+  expect_known_output(calc_summary_measures(samples), 
+                      file = "tests/test-data/calc_summary_measures_default.rds")
+})
+
+test_that("calc_CrI works as expected when grouping", {
+  expect_known_output(calc_summary_measures(samples, summarise_by = "type"),
+                      file = "tests/test-data/calc_summary_measures_grouping.rds")
+})
+
+
+test_that("calc_CrI works as expected when given a custom CrI list", {
+  expect_known_output(calc_summary_measures(samples, CrIs = c(0.1, 0.4, 0.95)),
+                      file = "tests/test-data/calc_summary_measures_custom_CrI.rds")
+})
