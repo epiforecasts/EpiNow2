0598131f01c6815a706db569d8c2a850e848c844
seabbs <s.e.abbott12@gmail.com>
2020-10-14 14:14:06 +0000

    add seed to test

--- tests/testthat/test-create_stan_args.R
+++ tests/testthat/test-create_stan_args.R
@@ -4,7 +4,7 @@ context("create_stan_args")
 test_that("create_stan_args returns the expected defaults when the exact method is used", {
 
   expect_equal(names(create_stan_args()), c("object", "data", "init", "refresh", "cores", "warmup",
-                                            "chains", "control", "save_warmup", "iter"))
+                                            "chains", "control", "save_warmup", "seed", "iter"))
 })
 
 test_that("create_stan_args returns the expected defaults when the approximate method is used", {
