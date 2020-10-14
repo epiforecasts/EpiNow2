24656a9e6e6de26dc8ef0f70220fdc49cf3a3ed1
seabbs <s.e.abbott12@gmail.com>
2020-10-14 22:39:54 +0000

    update news

--- tests/testthat/test-calc_CrI.R
+++ tests/testthat/test-calc_CrI.R
@@ -4,7 +4,7 @@ context("calc_CrI")
 samples <- data.frame(value = 1:10, type = "car")
 
 test_that("calc_CrI works as expected with default arguments", {
-  expect_known_output(calc_CrI(samples), file = "tests/test-data/calc_CrI_default.rds")
+  expect_known_output(calc_CrI(samples), file = "test-data/calc_CrI_default.rds")
 })
 
 test_that("calc_CrI works as expected when grouping", {
