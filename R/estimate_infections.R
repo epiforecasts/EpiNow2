99a6be142852029145ca14a77b192813e72d7cb0
seabbs <s.e.abbott12@gmail.com>
2020-10-23 13:06:53 +0000

    working version of convolve_to_report

--- R/estimate_infections.R
+++ R/estimate_infections.R
@@ -82,8 +82,8 @@
 #' # run model with default setting
 #' def <- estimate_infections(reported_cases, generation_time = generation_time,
 #'                            delays = list(incubation_period, reporting_delay), 
-#'                            stan_args = list(warmup = 200, control = list(adapt_delta = 0.8),
-#'                                             cores = ifelse(interactive(), 4, 1)), model = model, verbose = TRUE)
+#'                            stan_args = list(warmup = 200,
+#'                                             cores = ifelse(interactive(), 4, 1)))
 #'
 #' plots <- report_plots(summarised_estimates = def$summarised, reported = reported_cases)
 #' plots$summary
