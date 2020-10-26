4aad66dc23582bdf10ef5ef55cd0e9126bdffe9c
Sam Abbott <s.e.abbott12@gmail.com>
2020-10-26 18:28:27 +0000

    Update test-stan.R

--- inst/dev/test-stan.R
+++ inst/dev/test-stan.R
@@ -3,7 +3,6 @@ target_dir <- "inst/stan/functions"
 function_files <- c("pmfs.stan", "convolve.stan", "gaussian_process.stan",
                    "rt.stan", "infections.stan", "observation_model.stan",
                    "generated_quantities.stan")
-functions <- "functions/convolve.stan"
 functions <- paste0("\n functions{ \n",
                     paste(purrr::map_chr(function_files, 
                                    ~ paste(readLines(file.path(target_dir, .)), collapse = "\n")),
@@ -11,5 +10,3 @@ functions <- paste0("\n functions{ \n",
                     "\n }")
 
 rstan::expose_stan_functions(rstan::stanc(model_code = functions))
-
-convole
