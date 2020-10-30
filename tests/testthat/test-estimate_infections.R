49a6276c32fe4613b4ee1a5e3760d3231ae5ac4f
Michael DeWitt <michael.dewitt@conehealth.com>
2020-10-30 17:58:01 -0400

    interactive flag on cmdstan test. test local until figure out how to install cmdstanr on CI

--- tests/testthat/test-estimate_infections.R
+++ tests/testthat/test-estimate_infections.R
@@ -40,6 +40,7 @@ test_that("estimate_infections successfully returns estimates using default sett
 
 test_that("estimate_infections successfully returns estimates using default settings", {
   skip_on_cran()
+  testthat::skip_if_not(interactive())
   test_estimate_infections(reported_cases, stan_configuration = list(backend = "cmdstan"))
 })
 
