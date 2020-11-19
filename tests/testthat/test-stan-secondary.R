07ee6a8436017ccbe8adbae4622fbdc840fedb57
seabbs <s.e.abbott12@gmail.com>
2020-11-19 21:43:39 +0000

    wrap up testing of calculate_secondary

--- tests/testthat/test-stan-secondary.R
+++ tests/testthat/test-stan-secondary.R
@@ -1,19 +1,24 @@
 context("estimate_secondary")
 if (!testthat:::on_cran()) {
   files <- c("pmfs.stan", "convolve.stan", "observation_model.stan", "secondary.stan")
-  suppressMessages(expose_stan_fns(files, target_dir = "inst/stan/functions")) #system.file("stan/functions", package = "EpiNow2")))
+  suppressMessages(
+    expose_stan_fns(files, 
+                    target_dir = system.file("stan/functions", package = "EpiNow2")
+                    )
+    )
 }
 
-
+# test primary reports and observations
 reports <- rep(10, 20)
 obs <- rep(4, 20)
 
 check_equal <- function(args, target, dof = 0, dev = FALSE) {
   out <- do.call(calculate_secondary, args)
+  out <- round(out, dof)
   if (dev) {
     return(out)
   }
-  expect_equal(round(out, dof), target)
+  expect_equal(out, target)
 }
 
 test_that("calculate_secondary can calculate prevalence as expected", {
@@ -31,13 +36,20 @@ test_that("calculate_secondary can calculate incidence as expected", {
               target = c(1, 1, 1.5, rep(2.0, 17)), dof = 1)
 })
 
-# test only historic incidence
-calculate_secondary(reports, obs, 0.1, log(3), 0.1, 5, 0, 1, 1, 0, 1, 20) 
+test_that("calculate_secondary can calculate incidence using only historic reports", {
+  check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 1, 1, 0, 1, 20),
+              target = c(0, 0, rep(1, 18)), dof = 0)
+})
 
-# test only current incidence
-calculate_secondary(reports, obs, 0.1, log(3), 0.1, 5, 0, 0, 1, 1, 1, 20) 
+test_that("calculate_secondary can calculate incidence using only current reports", {
+  check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 0, 1, 1, 1, 20),
+              target = rep(1, 20), dof = 0)
+})
 
-# test prediction switch
-calculate_secondary(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 20)
-calculate_secondary(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 10) 
+test_that("calculate_secondary can switch into prediction mode as expected", {
+  check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 20),
+              target = c(1, rep(5, 19)), dof = 0)
+  check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 10) ,
+              target = 1:20, dof = 0)
+})
 
