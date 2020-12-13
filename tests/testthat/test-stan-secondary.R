fb56a3bbce317ceb69fb75d12d23780d780ca372
seabbs <s.e.abbott12@gmail.com>
2020-12-13 21:09:48 +0000

    fix test on stan

--- tests/testthat/test-stan-secondary.R
+++ tests/testthat/test-stan-secondary.R
@@ -22,31 +22,37 @@ check_equal <- function(args, target, dof = 0, dev = FALSE) {
 }
 
 test_that("calculate_secondary can calculate prevalence as expected", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 1, 1, 1, 1, 1, 20),
               target = c(1, 5, 5.5, rep(6, 17)), dof = 1)
 })
 
 test_that("calculate_secondary can calculate incidence as expected", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 1, 1, 1, 1, 20),
               target = c(1, 1, 1.5, rep(2.0, 17)), dof = 1)
 })
 
 test_that("calculate_secondary can calculate incidence as expected", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 1, 1, 1, 1, 20),
               target = c(1, 1, 1.5, rep(2.0, 17)), dof = 1)
 })
 
 test_that("calculate_secondary can calculate incidence using only historic reports", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 1, 1, 0, 1, 20),
               target = c(0, 0, rep(1, 18)), dof = 0)
 })
 
 test_that("calculate_secondary can calculate incidence using only current reports", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 0, 0, 1, 1, 1, 20),
               target = rep(1, 20), dof = 0)
 })
 
 test_that("calculate_secondary can switch into prediction mode as expected", {
+  skip_on_cran()
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 20),
               target = c(1, rep(5, 19)), dof = 0)
   check_equal(args = list(reports, obs, 0.1, log(3), 0.1, 5, 1, 0, 1, 1, 1, 10) ,
