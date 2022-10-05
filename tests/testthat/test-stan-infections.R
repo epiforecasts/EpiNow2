ed003a8d18c2bc2df36630b55835138a28a32906
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-10-05 15:47:54 +0100

    change expected tests results (rounding differences)

--- tests/testthat/test-stan-infections.R
+++ tests/testthat/test-stan-infections.R
@@ -3,49 +3,50 @@ skip_on_cran()
 # test update_infectiousness
 test_that("update_infectiousness works as expected with default settings", {
   expect_equal(
-    update_infectiousness(rep(1, 20), rep(0.1, 10), 5, 10, 10),
+    update_infectiousness(rep(1, 20), rep(0.1, 10), 5, 10),
     1
   )
   expect_equal(
-    update_infectiousness(rep(1, 20), rep(0.1, 5), 5, 5, 10),
+    update_infectiousness(rep(1, 20), rep(0.1, 5), 5, 10),
     0.5
   )
   expect_error(update_infectiousness(rep(1, 20), rep(0.1, 5), 5, 10, 10))
 })
 
+gt_pmf <- discretised_pmf(3, 2, 15, 1)
 
 # test generate infections
 test_that("generate_infections works as expected", {
   expect_equal(
-    round(generate_infections(c(1, rep(1, 9)), 10, 3, 2, 15, log(1000), 0, 0, 0), 0),
-    c(rep(1000, 10), 996, rep(997, 3), rep(998, 6))
+    round(generate_infections(c(1, rep(1, 9)), 10, gt_pmf, log(1000), 0, 0, 0), 0),
+    c(rep(1000, 10), 993, rep(996, 9))
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1.1, 9)), 10, 3, 2, 15, log(20), 0.03, 0, 0), 0),
-    c(20, 21, 21, 22, 23, 23, 24, 25, 25, 26, 25, 27, 28, 29, 30, 31, 32, 33, 35, 36)
+    round(generate_infections(c(1, rep(1.1, 9)), 10, gt_pmf, log(20), 0.03, 0, 0), 0),
+    c(20, 21, 21, 22, 23, 23, 24, 25, 25, 26, 24, 27, 28, 28, 29, 30, 31, 32, 32, 33)
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1.1, 9)), 10, 3, 2, 15, log(100), 0, 0, 0), 0),
-    c(rep(100, 11), 110, 113, 116, 120, 125, 129, 133, 138, 143)
+    round(generate_infections(c(1, rep(1.1, 9)), 10, gt_pmf, log(100), 0, 0, 0), 0),
+    c(rep(100, 10), 99, 110, 111, 114, 117, 120, 124, 127, 131, 134)
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1, 9)), 4, 3, 2, 15, log(500), -0.02, 0, 0), 0),
-    c(500, 490, 480, 471, 402, 413, 417, 417, rep(418, 6))
+    round(generate_infections(c(1, rep(1, 9)), 4, gt_pmf, log(500), -0.02, 0, 0), 0),
+    c(500, 490, 480, 471, 364, 397, 402, 403, rep(402, 4), rep(403, 2))
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1.1, 9)), 4, 3, 2, 15, log(500), 0, 0, 0), 0),
-    c(rep(500, 4), 416, 472, 490, 507, 524, 543, 562, 581, 601, 622)
+    round(generate_infections(c(1, rep(1.1, 9)), 4, gt_pmf, log(500), 0, 0, 0), 0),
+    c(rep(500, 4), 375, 452, 464, 477, 490, 504, 518, 533, 547, 563)
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1, 9)), 1, 3, 2, 15, log(40), numeric(0), 0, 0), 0),
-    c(40, 11, 13, rep(14, 8))
+    round(generate_infections(c(1, rep(1, 9)), 1, gt_pmf, log(40), numeric(0), 0, 0), 0),
+    c(40, 5, 10, rep(11, 8))
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1.1, 9)), 1, 3, 2, 15, log(100), 0.01, 0, 0), 0),
-    c(100, 28, 36, 39, 41, 42, 44, 45, 47, 48, 50)
+    round(generate_infections(c(1, rep(1.1, 9)), 1, gt_pmf, log(100), 0.01, 0, 0), 0),
+    c(100, 12, 28, 32, rep(33, 2), 34, 35, 36, 37, 38)
   )
   expect_equal(
-    round(generate_infections(c(1, rep(1, 9)), 10, 3, 2, 15, log(1000), 0, 100000, 4), 0),
-    c(rep(1000, 10), 996, 997, 997, 997, 998, 998, 980, 964, 945, 922)
+    round(generate_infections(c(1, rep(1, 9)), 10, gt_pmf, log(1000), 0, 100000, 4), 0),
+    c(rep(1000, 10), 993, rep(996, 5), 979, 965, 949, 929)
   )
 })
