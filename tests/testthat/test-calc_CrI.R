024aece2dfad78c76644b40fe09ec8d3178468ac
Sam Abbott <contact@samabbott.co.uk>
2022-10-15 18:20:30 +0000

    update depreciated testthat features

--- tests/testthat/test-calc_CrI.R
+++ tests/testthat/test-calc_CrI.R
@@ -1,13 +1,13 @@
 samples <- data.frame(value = 1:10, type = "car")
 
 test_that("calc_CrI works as expected with default arguments", {
-  expect_known_output(calc_CrI(samples),
+  expect_snapshot_output(calc_CrI(samples),
     file = testthat::test_path("test-data/calc_CrI_default.rds")
   )
 })
 
 test_that("calc_CrI works as expected when grouping", {
-  expect_known_output(calc_CrI(samples, summarise_by = "type"),
+  expect_snapshot_output(calc_CrI(samples, summarise_by = "type"),
     file = testthat::test_path("test-data/calc_CrI_grouping.rds")
   )
 })
--- tests/testthat/test-calc_summary_stats.R
+++ tests/testthat/test-calc_summary_stats.R
@@ -1,13 +1,13 @@
 samples <- data.frame(value = 1:10, type = "car")
 
 test_that("calc_summary_stats works as expected with default arguments", {
-  expect_known_output(calc_summary_stats(samples),
+  expect_snapshot_output(calc_summary_stats(samples),
     file = testthat::test_path("test-data/calc_summary_stats_default.rds")
   )
 })
 
 test_that("calc_summary_stats works as expected when grouping", {
-  expect_known_output(calc_summary_stats(samples, summarise_by = "type"),
+  expect_snapshot_output(calc_summary_stats(samples, summarise_by = "type"),
     file = testthat::test_path("test-data/calc_summary_stats_grouping.rds")
   )
 })
