epiforecasts/EpiNow2

commit fe7675f39738346aaea8b4a1e65fdcf825bf3fd7

add more flexibility with delays (#305)

R/estimate_truncation.R
@@ -31,7 +31,10 @@
 #' and a confirm (integer) variable. Each data set should be a snapshot
 #' of the reported data over time. All data sets must contain a complete vector
 #' of dates.
-#' @param max_truncation Integer, defaults to 10. Maximum number of
+#' @param trunc_max Integer, defaults to 10. Maximum number of
+#' days to include in the truncation distribution.
+#' @param trunc_dist Character, defaults to "lognormal". The parametric
+#' distribution to be used for truncation.
 #' days to include in the truncation distribution.
 #' @param model A compiled stan model to override the default model. May be
 #' useful for package developers or those developing extensions.
@@ -57,7 +60,7 @@
 #' reported_cases <- example_confirmed[1:60]
 #'
 #' # define example truncation distribution (note not integer adjusted)
-#' trunc_dist <- list(
+#' trunc <- list(
 #'   mean = convert_to_logmean(3, 2),
 #'   mean_sd = 0.1,
 #'   sd = convert_to_logsd(3, 2),
@@ -84,7 +87,7 @@
 #' example_data <- purrr::map(c(20, 15, 10, 0),
 #'   construct_truncation,
 #'   cases = reported_cases,
-#'   dist = trunc_dist
+#'   dist = trunc
 #' )
 #'
 #' # fit model to example data
@@ -101,13 +104,16 @@
 #' print(est$obs)
 #' # validation plot of observations vs estimates
 #' plot(est)
-#' 
+#'
 #' options(old_opts)
-estimate_truncation <- function(obs, max_truncation = 10,
+estimate_truncation <- function(obs, trunc_max = 10,
+                                trunc_dist = c("lognormal", "gamma"),
                                 model = NULL,
                                 CrIs = c(0.2, 0.5, 0.9),
                                 verbose = TRUE,
                                 ...) {
+  trunc_dist <- match.arg(trunc_dist)
+
   # combine into ordered matrix
   dirty_obs <- purrr::map(obs, data.table::as.data.table)
   nrow_obs <- order(purrr::map_dbl(dirty_obs, nrow))
@@ -118,7 +124,7 @@ estimate_truncation <- function(obs, max_truncation = 10,
     confirm := NULL
   ])
   obs <- purrr::reduce(obs, merge, all = TRUE)
-  obs_start <- nrow(obs) - max_truncation - sum(is.na(obs$`1`)) + 1
+  obs_start <- nrow(obs) - trunc_max - sum(is.na(obs$`1`)) + 1
   obs_dist <- purrr::map_dbl(2:(ncol(obs)), ~ sum(is.na(obs[[.]])))
   obs_data <- obs[, -1][, purrr::map(.SD, ~ ifelse(is.na(.), 0, .))]
   obs_data <- obs_data[obs_start:.N]
@@ -129,8 +135,13 @@ estimate_truncation <- function(obs, max_truncation = 10,
     obs_dist = obs_dist,
     t = nrow(obs_data),
     obs_sets = ncol(obs_data),
-    trunc_max = max_truncation
+    trunc_max = trunc_max,
+    trunc_dist = trunc_dist
   )
+
+  ## convert to integer
+  data$trunc_dist <-
+    which(eval(formals()[["trunc_dist"]]) == trunc_dist) - 1
 
   # initial conditions
   init_fn <- function() {
@@ -161,7 +172,7 @@ estimate_truncation <- function(obs, max_truncation = 10,
     mean_sd = round(rstan::summary(fit, pars = "logmean")$summary[3], 3),
     sd = round(rstan::summary(fit, pars = "logsd")$summary[1], 3),
     sd_sd = round(rstan::summary(fit, pars = "logsd")$summary[3], 3),
-    max = max_truncation
+    max = trunc_max
   )
 
   # summarise reconstructed observations
@@ -184,7 +195,7 @@ estimate_truncation <- function(obs, max_truncation = 10,
     ]
   link_obs <- function(index) {
     target_obs <- dirty_obs[[index]][, index := .N - 0:(.N - 1)]
-    target_obs <- target_obs[index < max_truncation]
+    target_obs <- target_obs[index < trunc_max]
     estimates <- recon_obs[dataset == index][, c("id", "dataset") := NULL]
     estimates <- estimates[, lapply(.SD, as.integer)]
     estimates <- estimates[, index := .N - 0:(.N - 1)]
@@ -194,7 +205,7 @@ estimate_truncation <- function(obs, max_truncation = 10,
     if (!is.null(estimates$Rhat)) {
       estimates[, c("Rhat") := NULL]
     }
-    
+
     target_obs <-
       data.table::merge.data.table(
         target_obs, last_obs,
@@ -248,7 +259,7 @@ plot.estimate_truncation <- function(x, ...) {
     ggplot2::facet_wrap(~report_date, scales = "free")
 
   plot <- plot_CrIs(plot, extract_CrIs(x$obs),
-    alpha = 0.8, size = 1
+    alpha = 0.8, linewidth = 1
   )
 
   plot <- plot +

R/opts.R
@@ -1,69 +1,187 @@
+#' Generation Time Distribution Options
+#'
+#' @description `r lifecycle::badge("stable")`
+#' Returns generation time parameters in a format for lower level model use. The
+#' generation time can either be given as a \code{disease} and \code{source} to
+#' be passed to [get_generation_time], or as parameters of a distribution to be
+#' passed to [delay_dist].
+#' @param ... Any parameters to be passed to [delay_dist], if the generation time
+#' is given as parameters of a distribution rather than a \code{disease} and
+#' \code{source}. In this case if the \code{mean} parameter is not set a mean of
+#' 1 will be assumed, if the \code{max} parameter not set then the \code{max} will
+#' be set to 15 to ensure backwards compatibility, and if no \code{dist} parameter
+#' is given then a gamma distribution will be used for backwards compatibility.
+#' @param max Integer, defaults to 15. Maximum generation time.
+#' @param fixed Logical, defaults to `FALSE`. Should the generation time be
+#' treated as coming from fixed (vs uncertain) distributions.
+#' @param prior_weight numeric, weight given to the generation time prior.
+#' By default the generation time prior will be weighted by the number of
+#' observation data points, usually preventing the posteriors from shifting
+#' much from the given distribution. Another sensible option would be 1,
+#' i.e. treating the generation time distribution as a single parameter.
+#' @inheritParams get_generation_time
+#' @seealso convert_to_logmean convert_to_logsd bootstrapped_dist_fit delay_dist
+#' @return A list summarising the input delay distributions.
+#' @export
+#' @examples
+#' # default settings with a fixed generation time
+#' generation_time_opts()
+#'
+#' # A fixed gamma distributed generation time
+#' generation_time_opts(mean = 3, sd = 2)
+#'
+#' # An uncertain gamma distributed generation time
+#' generation_time_opts(mean = 3, sd = 2, mean_sd = 1, sd_sd = 0.5)
+generation_time_opts <- function(..., disease, source, max = 15L, fixed = FALSE, prior_weight = NULL) {
+  dot_options <- list(...) ## options for delay_dist
+  ## check consistent options are given
+  type_options <- (length(dot_options) > 0) + ## distributional parameters
+    (!missing(disease) && !missing(source)) ## from included distributions
+  if (type_options > 1)  {
+    stop("Generation time should be given either as distributional options  ",
+         "or as disease/source, but not both.")
+  }
+
+  if (!missing(disease) && !missing(source)) { ## generation time provided as disease/source
+    dist <- get_generation_time(
+      disease = disease, source = source, max_value = max
+    )
+    dist$fixed <- fixed
+    gt <- do.call(delay_dist, dist)
+  } else { ## generation time provided as distributional parameters or not at all
+    ## make gamma default for backwards compatibility
+    if (!("dist" %in% names(dot_options))) {
+      dot_options$dist <- "gamma"
+    }
+    ## set max
+    dot_options$max <- max
+    ## set default of mean=1 for backwards compatibility
+    if (!("mean" %in% names(dot_options))) {
+      dot_options$mean <- 1
+    }
+    dot_options$fixed <- fixed
+    gt <- do.call(delay_dist, dot_options)
+  }
+  gt$weight <- prior_weight
+
+  names(gt) <- paste0("gt_", names(gt))
+
+  return(gt)
+}
+
 #' Delay Distribution Options
 #'
 #' @description `r lifecycle::badge("stable")`
 #' Returns delay distributions formatted for usage by downstream
 #' functions.
-#' @param ... Delay distributions as a list with the following parameters:
-#' "mean", "mean_sd", "sd_mean", "sd_sd", and "max" defining a truncated log
-#' normal (with all parameters except for max defined in logged form).
+#' @param ... Parameters of discretised (upper-)truncated lognormal delay
+#' distributions as a list with the following parameters:
+#' "mean", the mu parameter or mean of the natural logarithm of the delay;
+#' "mean_sd", the standard deviation in the estimate of "mean" parameter
+#' (assumed normally distributed); "sd", the sigma parameter or standard
+#' deviation of the natural logarithm of the delay; "sd_sd", the standard
+#' deviation of the estimate of the "sd" parameter (assumed normally
+#' distributed) sd_sd"; and "max", the maximum delay.
+#' The "mean" parameter is mandatory; if it is the only one given it represents
+#' a fixed delay and must be integer-valued; if "sd" is also given and
+#' greater than 0 this represents a delay distribution and "mean" can be
+#' real-valued. In that case, "max" also needs to be given.
+#' The "mean_sd" and "sd_sd" parameters should be provided to represent
+#' uncertainty in the parameter values of the delay but are optional.
+#' @param fixed Logical, defaults to `FALSE`. Should all reporting delays be
+#' treated as coming from fixed (vs uncertain) distributions. Making this
+#' simplification drastically reduces compute requirements. Setting this here
+#' overrides any of the constituent delay distributions being set to be fixed
+#' or not.
 #' @seealso convert_to_logmean convert_to_logsd bootstrapped_dist_fit
 #' @return A list summarising the input delay distributions.
 #' @export
 #' @examples
 #' # no delays
 #' delay_opts()
-delay_opts <- function(...) {
+#'
+#' # A single delay that has uncertainty
+#' delay <- list(mean = 1, mean_sd = 0.2, sd = 0.5, sd_sd = 0.1, max = 15)
+#' delay_opts(delay)
+#'
+#' # A single delay where we override the uncertainty assumption
+#' delay_opts(delay, fixed = TRUE)
+#'
+#' # A delay where uncertainty is implict
+#' delay_opts(list(mean = 1, mean_sd = 0, sd = 0.5, sd_sd = 0, max = 15))
+delay_opts <- function(..., fixed = FALSE) {
   delays <- list(...)
-  data <- list()
-  data$delays <- length(delays)
-  if (data$delays > 0) {
-    delays <- purrr::transpose(delays)
+  data <- lapply(delays, do.call, what = delay_dist)
+
+  if (fixed) { ## set all to fixed
+    data <- lapply(data, function(x) {
+      x$fixed <- 1L
+      x$mean_sd <- 0
+      x$sd_sd <- 0
+      return(x)
+    })
   }
 
-  # Estimate the mean delay -----------------------------------------------
-  if (data$delays > 0) {
-    data$seeding_time <- sum(
-      purrr::map2_dbl(delays$mean, delays$sd, ~ exp(.x + .y^2 / 2))
-    )
-    if (data$seeding_time < 1) {
-      data$seeding_time <- 1
-    } else {
-      data$seeding_time <- as.integer(data$seeding_time)
-    }
+  if (length(data) > 0) {
+    data <- purrr::transpose(data)
+    ## convert back to arrays
+    data <- lapply(data, function(x) array(unlist(x)))
   } else {
-    data$seeding_time <- 1
+    data <- delay_dist()
   }
-  data$delay_mean_mean <- allocate_delays(delays$mean, data$delays)
-  data$delay_mean_sd <- allocate_delays(delays$mean_sd, data$delays)
-  data$delay_sd_mean <- allocate_delays(delays$sd, data$delays)
-  data$delay_sd_sd <- allocate_delays(delays$sd_sd, data$delays)
-  data$max_delay <- allocate_delays(delays$max, data$delays)
+
+  names(data) <- paste0("delay_", names(data))
+  # Estimate the mean delay -----------------------------------------------
+  data$seeding_time <- sum(
+    purrr::map2_dbl(data$mean_mean, data$sd_mean, ~ exp(.x + .y^2 / 2))
+  )
+  if (data$seeding_time < 1) {
+    data$seeding_time <- 1
+  } else {
+    data$seeding_time <- as.integer(data$seeding_time)
+  }
+
+  data$delays <- length(delays)
+
+  data$uncertain_mean_delays <- array(which(data$delay_mean_sd > 0))
+  data$uncertain_sd_delays <- array(which(data$delay_sd_sd > 0))
+  data$fixed_delays <- array(
+    which(data$delay_mean_sd == 0 & data$delay_sd_sd == 0)
+  )
+
+  data$n_uncertain_mean_delays <- length(data$uncertain_mean_delays)
+  data$n_uncertain_sd_delays <- length(data$uncertain_sd_delays)
+  data$n_fixed_delays <- length(data$fixed_delays)
+
   return(data)
 }
 
 #' Truncation Distribution Options
 #'
 #' @description `r lifecycle::badge("stable")`
-#' Returns a truncation distribution formatted for usage by downstream functions. See
-#' `estimate_truncation` for an approach to estimate this distribution.
-#' @param dist A list defining the truncation distribution, defaults to `NULL` in which
-#' case no truncation is used. Must have the following elements if defined: "mean", "mean_sd",
-#' "sd_mean", "sd_sd", and "max" defining a truncated log normal (with all parameters except
-#' for max defined in logged form).
-#' @seealso convert_to_logmean convert_to_logsd bootstrapped_dist_fit
+#' Returns a truncation distribution formatted for usage by
+#'  downstream functions. See `estimate_truncation()` for an approach to
+#'  estimate this distribution.
+#' @param ... Any parameters to be passed to [delay_dist]. If the `max` parameter
+#' is not set but other distributional parameters given then the `max` will be
+#' set to 15 to ensure backwards compatibility. Also if no `dist` parameter is given
+#' then a gamma distribution will be used for backwards compatibility.
+#' @seealso convert_to_logmean convert_to_logsd bootstrapped_dist_fit delay_dist
 #' @return A list summarising the input truncation distribution.
 #' @export
 #' @examples
 #' # no truncation
 #' trunc_opts()
-trunc_opts <- function(dist = NULL) {
-  data <- list()
-  data$truncation <- ifelse(is.null(dist), 0, 1)
-  data$trunc_mean_mean <- allocate_delays(dist$mean, data$truncation)
-  data$trunc_mean_sd <- allocate_delays(dist$mean_sd, data$truncation)
-  data$trunc_sd_mean <- allocate_delays(dist$sd, data$truncation)
-  data$trunc_sd_sd <- allocate_delays(dist$sd_sd, data$truncation)
-  data$max_truncation <- allocate_delays(dist$max, data$truncation)
+#'
+#' # truncation dist
+#' trunc_opts(mean = 3, sd = 2)
+trunc_opts <- function(...) {
+  dot_options <- list(...) ## options for delay_dist
+  present <- (length(dot_options) > 0)
+  data <- do.call(delay_dist, dot_options)
+  names(data) <- paste0("trunc_", names(data))
+  data$truncation <- as.integer(present)
+
   return(data)
 }
 
@@ -238,7 +356,7 @@ gp_opts <- function(basis_prop = 0.2,
 #' model. Custom settings can be supplied which override the defaults.
 #' @param family Character string defining the observation model. Options are
 #' Negative binomial ("negbin"), the default, and Poisson.
-#' @param phi A numeric vector of length 2, defaults to 0, 1. Indicates the 
+#' @param phi A numeric vector of length 2, defaults to 0, 1. Indicates the
 #' mean and standard deviation of the normal prior used for the observation
 #' process.
 #' @param weight Numeric, defaults to 1. Weight to give the observed data in

man/trunc_opts.Rd
@@ -4,26 +4,30 @@
 \alias{trunc_opts}
 \title{Truncation Distribution Options}
 \usage{
-trunc_opts(dist = NULL)
+trunc_opts(...)
 }
 \arguments{
-\item{dist}{A list defining the truncation distribution, defaults to \code{NULL} in which
-case no truncation is used. Must have the following elements if defined: "mean", "mean_sd",
-"sd_mean", "sd_sd", and "max" defining a truncated log normal (with all parameters except
-for max defined in logged form).}
+\item{...}{Any parameters to be passed to \link{delay_dist}. If the \code{max} parameter
+is not set but other distributional parameters given then the \code{max} will be
+set to 15 to ensure backwards compatibility. Also if no \code{dist} parameter is given
+then a gamma distribution will be used for backwards compatibility.}
 }
 \value{
 A list summarising the input truncation distribution.
 }
 \description{
 \ifelse{html}{\href{https://lifecycle.r-lib.org/articles/stages.html#stable}{\figure{lifecycle-stable.svg}{options: alt='[Stable]'}}}{\strong{[Stable]}}
-Returns a truncation distribution formatted for usage by downstream functions. See
-\code{estimate_truncation} for an approach to estimate this distribution.
+Returns a truncation distribution formatted for usage by
+downstream functions. See \code{estimate_truncation()} for an approach to
+estimate this distribution.
 }
 \examples{
 # no truncation
 trunc_opts()
+
+# truncation dist
+trunc_opts(mean = 3, sd = 2)
 }
 \seealso{
-convert_to_logmean convert_to_logsd bootstrapped_dist_fit
+convert_to_logmean convert_to_logsd bootstrapped_dist_fit delay_dist
 }

tests/testthat/test-delays.R
@@ -1,0 +1,77 @@
+test_stan_data <- function(generation_time = generation_time_opts(),
+                           delays = delay_opts(),
+                           truncation = trunc_opts(),
+                           params = c()) {
+  data <- create_stan_data(
+    reported_cases = example_confirmed,
+    generation_time = generation_time,
+    delays = delays,
+    truncation = truncation,
+    rt = rt_opts(),
+    gp = gp_opts(),
+    obs = obs_opts(),
+    backcalc = backcalc_opts(),
+    shifted_cases = NULL,
+    horizon = 7
+  )
+  return(unlist(unname(data[params])))
+}
+
+test_that("generation times can be specified in different ways", {
+  gt_params <-
+    c("gt_mean_mean", "gt_mean_sd", "gt_sd_mean", "gt_sd_sd", "gt_max")
+  expect_equal(
+    test_stan_data(params = gt_params),
+    c(1, 0, 0, 0, 1)
+  )
+  expect_equal(
+    test_stan_data(generation_time = generation_time_opts(mean = 3),
+                   params = gt_params),
+    c(3, 0, 0, 0, 3)
+  )
+  expect_equal(
+    test_stan_data(generation_time = generation_time_opts(mean = 3, sd = 1, max = 5),
+                   params = gt_params),
+    c(3, 0, 1, 0, 5)
+  )
+})
+
+test_that("delay parameters can be specified in different ways", {
+  delay_params <-
+    c("delay_mean_mean", "delay_mean_sd", "delay_sd_mean", "delay_sd_sd",
+      "delay_max")
+  expect_equal(
+    test_stan_data(delays = delay_opts(list(mean = 3)),
+                   params = delay_params),
+    c(3, 0, 0, 0, 3)
+  )
+  expect_equal(
+    test_stan_data(delays = delay_opts(list(mean = 3, sd = 1, max = 5)),
+                   params = delay_params),
+    c(3, 0, 1, 0, 5)
+  )
+})
+
+test_that("truncation parameters can be specified in different ways", {
+  trunc_params <-
+    c("trunc_mean_mean", "trunc_mean_sd", "trunc_sd_mean", "trunc_sd_sd",
+      "trunc_max")
+  expect_equal(
+    test_stan_data(truncation = trunc_opts(mean = 3, sd = 1, max = 5),
+                   params = trunc_params),
+    c(3, 0, 1, 0, 5)
+  )
+})
+
+test_that("contradictory generation times are caught", {
+  expect_error(generation_time_opts(mean = 3.5), "must be an integer")
+  expect_error(generation_time_opts(mean = 3, mean_sd = 1),
+               "must be 0")
+})
+
+test_that("contradictory delays are caught", {
+  expect_error(test_stan_data(delays = delay_opts(list(mean = 3.5))),
+               "must be an integer")
+  expect_error(test_stan_data(delays = delay_opts(list(mean = 3, mean_sd = 1))),
+               "must be 0")
+})

tests/testthat/test-simulate_infections.R
@@ -52,7 +52,7 @@ test_that("simulate infections fails as expected", {
 })
 
 test_that("simulate_infections works to simulate a passed in estimate_infections object with an adjusted Rt in data frame", {
-  R <- c(rep(NA_real_, 32), rep(0.5, 17))
+  R <- c(rep(NA_real_, 31), rep(0.5, 17))
   R_dt <- data.frame(date = summary(out, type = "parameters", param = "R")$date, value = R)
   sims_dt <- simulate_infections(out, R_dt)
   expect_equal(names(sims_dt), c("samples", "summarised", "observations"))

DESCRIPTION
@@ -137,6 +137,6 @@ Encoding: UTF-8
 Language: en-GB
 LazyData: true
 Roxygen: list(markdown = TRUE)
-RoxygenNote: 7.2.1
+RoxygenNote: 7.2.2
 SystemRequirements: GNU make
 VignetteBuilder: knitr

NAMESPACE
@@ -29,6 +29,7 @@ export(create_rt_data)
 export(create_shifted_cases)
 export(create_stan_args)
 export(create_stan_data)
+export(delay_dist)
 export(delay_opts)
 export(dist_fit)
 export(dist_skel)
@@ -44,6 +45,7 @@ export(extract_inits)
 export(extract_stan_param)
 export(forecast_secondary)
 export(gamma_dist_def)
+export(generation_time_opts)
 export(get_dist)
 export(get_generation_time)
 export(get_incubation_period)

R/create.R
@@ -395,33 +395,13 @@ create_stan_data <- function(reported_cases, generation_time,
                              backcalc, shifted_cases,
                              truncation) {
 
-  ## make sure we have at least max_gt seeding time
+  ## make sure we have at least gt_max seeding time
   delays$seeding_time <- max(delays$seeding_time, generation_time$max)
 
-  ## complete generation time parameters if not all are given
-  if (is.null(generation_time)) {
-    generation_time <- list(mean = 1)
-  }
-  for (param in c("mean_sd", "sd", "sd_sd")) {
-    if (!(param %in% names(generation_time))) generation_time[[param]] <- 0
-  }
-  ## check if generation time is fixed
-  if (generation_time$sd == 0 && generation_time$sd_sd == 0) {
-    if ("max_gt" %in% names(generation_time)) {
-      if (generation_time$max_gt != generation_time$mean) {
-        stop("Error in generation time defintion: if max_gt(",
-             generation_time$max_gt,
-             ") is given it must be equal to the mean (",
-             generation_time$mean,
-             ")")
-      }
-    } else {
-      generation_time$max_gt <- generation_time$mean
-    }
-    if (any(generation_time$mean_sd > 0, generation_time$sd_sd > 0)) {
-      stop("Error in generation time definition: if sd_mean is 0 and ",
-           "sd_sd is 0 then mean_sd must be 0, too.")
-    }
+  ## for backwards compatibility call generation_time_opts internally
+  if (is.list(generation_time) &&
+      all(c("mean", "mean_sd", "sd", "sd_sd") %in% names(generation_time))) {
+    generation_time <- do.call(generation_time_opts, generation_time)
   }
 
   cases <- reported_cases[(delays$seeding_time + 1):(.N - horizon)]$confirm
@@ -431,13 +411,10 @@ create_stan_data <- function(reported_cases, generation_time,
     shifted_cases = shifted_cases,
     t = length(reported_cases$date),
     horizon = horizon,
-    gt_mean_mean = generation_time$mean,
-    gt_mean_sd = generation_time$mean_sd,
-    gt_sd_mean = generation_time$sd,
-    gt_sd_sd = generation_time$sd_sd,
-    max_gt = generation_time$max,
     burn_in = 0
   )
+  # add gt data
+  data <- c(data, generation_time)
   # add delay data
   data <- c(data, delays)
   # add truncation data
@@ -459,6 +436,10 @@ create_stan_data <- function(reported_cases, generation_time,
   data$prior_infections <- ifelse(is.na(data$prior_infections) | is.null(data$prior_infections),
     0, data$prior_infections
   )
+  if (is.null(data$gt_weight)) {
+    ## default: weigh by number of data points
+    data$gt_weight <- data$t - data$seeding_time - data$horizon
+  }
   if (data$seeding_time > 1) {
     safe_lm <- purrr::safely(stats::lm)
     data$prior_growth <- safe_lm(log(confirm) ~ t, data = first_week)[[1]]
@@ -503,26 +484,35 @@ create_stan_data <- function(reported_cases, generation_time,
 create_initial_conditions <- function(data) {
   init_fun <- function() {
     out <- list()
-    if (data$delays > 0) {
+    if (data$n_uncertain_mean_delays > 0) {
       out$delay_mean <- array(purrr::map2_dbl(
-        data$delay_mean_mean, data$delay_mean_sd * 0.1,
+        data$delay_mean_mean[data$uncertain_mean_delays],
+        data$delay_mean_sd[data$uncertain_mean_delays] * 0.1,
         ~ rnorm(1, mean = .x, sd = .y)
       ))
+    }
+    if (data$n_uncertain_sd_delays > 0) {
       out$delay_sd <- array(purrr::map2_dbl(
-        data$delay_sd_mean, data$delay_sd_sd * 0.1,
+        data$delay_sd_mean[data$uncertain_sd_delays],
+        data$delay_sd_sd[data$uncertain_sd_delays] * 0.1,
         ~ rnorm(1, mean = .x, sd = .y)
       ))
     }
     if (data$truncation > 0) {
-      out$truncation_mean <- array(rnorm(1,
-        mean = data$trunc_mean_mean,
-        sd = data$trunc_mean_sd * 0.1
-      ))
-      out$truncation_sd <- array(truncnorm::rtruncnorm(1,
-        a = 0,
-        mean = data$trunc_sd_mean,
-        sd = data$trunc_sd_sd * 0.1
-      ))
+     if (data$trunc_mean_sd > 0) {
+       out$truncation_mean <- array(rnorm(1,
+         mean = data$trunc_mean_mean,
+         sd = data$trunc_mean_sd * 0.1
+       ))
+     }
+     if (data$trunc_sd_sd > 0) {
+       out$truncation_sd <- array(
+         truncnorm::rtruncnorm(1,
+           a = 0,
+           mean = data$trunc_sd_mean,
+           sd = data$trunc_sd_sd * 0.1
+         ))
+      }
     }
     if (data$fixed == 0) {
       out$eta <- array(rnorm(data$M, mean = 0, sd = 0.1))
@@ -577,6 +567,9 @@ create_initial_conditions <- function(data) {
         mean = data$obs_scale_mean,
         sd = data$obs_scale_sd * 0.1
       ))
+    }
+    if (data$week_effect > 0) {
+      out$day_of_week_simplex = array(rep(1 / data$week_effect, data$week_effect))
     }
     return(out)
   }

commit 86d0eeda6a47038b3e9add8169f2f4ae73e8fb98

fixes to small issues introduced by PR #305 (#336)

R/estimate_truncation.R
@@ -229,7 +229,7 @@ estimate_truncation <- function(obs, max_truncation, trunc_max = 10,
   out$last_obs <- last_obs
   # summarise estimated cmf of the truncation distribution
   out$cmf <- extract_stan_param(fit, "cmf", CrIs = CrIs)
-  out$cmf <- data.table::as.data.table(out$cmf)[, index := .N:1]
+  out$cmf <- data.table::as.data.table(out$cmf)[, index := seq_len(.N)]
   data.table::setcolorder(out$cmf, "index")
   out$data <- data
   out$fit <- fit

R/opts.R
@@ -132,8 +132,8 @@ delay_opts <- function(..., fixed = FALSE) {
 
   names(data) <- paste0("delay_", names(data))
   # Estimate the mean delay -----------------------------------------------
-  data$seeding_time <- sum(
-    purrr::map2_dbl(data$mean_mean, data$sd_mean, ~ exp(.x + .y^2 / 2))
+  data$seeding_time <- sum(purrr::map2_dbl(
+    data$delay_mean_mean, data$delay_sd_mean, ~ exp(.x + .y^2 / 2))
   )
   if (data$seeding_time < 1) {
     data$seeding_time <- 1
@@ -162,10 +162,9 @@ delay_opts <- function(..., fixed = FALSE) {
 #' Returns a truncation distribution formatted for usage by
 #'  downstream functions. See `estimate_truncation()` for an approach to
 #'  estimate this distribution.
-#' @param ... Any parameters to be passed to [delay_dist]. If the `max` parameter
-#' is not set but other distributional parameters given then the `max` will be
-#' set to 15 to ensure backwards compatibility. Also if no `dist` parameter is given
-#' then a gamma distribution will be used for backwards compatibility.
+#' @param dist Parameters of a discretised (upper-)truncated lognormal
+#' truncation distribution as a list with parameters that are all passed to
+#' [delay_dist].
 #' @seealso convert_to_logmean convert_to_logsd bootstrapped_dist_fit delay_dist
 #' @return A list summarising the input truncation distribution.
 #' @export
@@ -174,14 +173,11 @@ delay_opts <- function(..., fixed = FALSE) {
 #' trunc_opts()
 #'
 #' # truncation dist
-#' trunc_opts(mean = 3, sd = 2)
-trunc_opts <- function(...) {
-  dot_options <- list(...) ## options for delay_dist
-  present <- (length(dot_options) > 0)
-  data <- do.call(delay_dist, dot_options)
+#' trunc_opts(dist = list(mean = 3, sd = 2))
+trunc_opts <- function(dist = list()) {
+  data <- do.call(delay_dist, dist)
   names(data) <- paste0("trunc_", names(data))
-  data$truncation <- as.integer(present)
-
+  data$truncation <- as.integer(length(data$trunc_max) > 0)
   return(data)
 }
 

man/trunc_opts.Rd
@@ -4,13 +4,12 @@
 \alias{trunc_opts}
 \title{Truncation Distribution Options}
 \usage{
-trunc_opts(...)
+trunc_opts(dist = list())
 }
 \arguments{
-\item{...}{Any parameters to be passed to \link{delay_dist}. If the \code{max} parameter
-is not set but other distributional parameters given then the \code{max} will be
-set to 15 to ensure backwards compatibility. Also if no \code{dist} parameter is given
-then a gamma distribution will be used for backwards compatibility.}
+\item{dist}{Parameters of a discretised (upper-)truncated lognormal
+truncation distribution as a list with parameters that are all passed to
+\link{delay_dist}.}
 }
 \value{
 A list summarising the input truncation distribution.
@@ -26,7 +25,7 @@ estimate this distribution.
 trunc_opts()
 
 # truncation dist
-trunc_opts(mean = 3, sd = 2)
+trunc_opts(dist = list(mean = 3, sd = 2))
 }
 \seealso{
 convert_to_logmean convert_to_logsd bootstrapped_dist_fit delay_dist

tests/testthat/test-delays.R
@@ -57,8 +57,10 @@ test_that("truncation parameters can be specified in different ways", {
     c("trunc_mean_mean", "trunc_mean_sd", "trunc_sd_mean", "trunc_sd_sd",
       "trunc_max")
   expect_equal(
-    test_stan_data(truncation = trunc_opts(mean = 3, sd = 1, max = 5),
-                   params = trunc_params),
+    test_stan_data(
+      truncation = trunc_opts(dist =  list(mean = 3, sd = 1, max = 5)),
+      params = trunc_params
+    ),
     c(3, 0, 1, 0, 5)
   )
 })

tests/testthat/test-simulate_infections.R
@@ -52,7 +52,7 @@ test_that("simulate infections fails as expected", {
 })
 
 test_that("simulate_infections works to simulate a passed in estimate_infections object with an adjusted Rt in data frame", {
-  R <- c(rep(NA_real_, 31), rep(0.5, 17))
+  R <- c(rep(NA_real_, 32), rep(0.5, 17))
   R_dt <- data.frame(date = summary(out, type = "parameters", param = "R")$date, value = R)
   sims_dt <- simulate_infections(out, R_dt)
   expect_equal(names(sims_dt), c("samples", "summarised", "observations"))