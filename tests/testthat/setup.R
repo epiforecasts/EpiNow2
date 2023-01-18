83c1f50ad894bb7ad42f7e15c272df638924c9a5
Sam <s.e.abbott12@gmail.com>
2023-01-18 21:06:55 +0000

    Hotfix: Do not test internal stan functions on windwos due to Rtools compilation issue

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,11 +3,13 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
     "pmfs.stan", "convolve.stan", "observation_model.stan", "secondary.stan",
     "rt.stan", "infections.stan"
   )
-  suppressMessages(
-    expose_stan_fns(files,
-      target_dir = system.file("stan/functions", package = "EpiNow2")
+  if (!(tolower(Sys.info()[["sysname"]]) %in% "windows")) {
+    suppressMessages(
+      expose_stan_fns(files,
+        target_dir = system.file("stan/functions", package = "EpiNow2")
+      )
     )
-  )
+  }
 }
 
 withr::defer(future::plan("sequential"), teardown_env())
