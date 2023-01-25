40bfb840e6b485d363ebb823a8fa5fa880271a10
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-01-25 10:51:04 +0000

    update synthetic validation

--- inst/dev/recover-synthetic/rt.R
+++ inst/dev/recover-synthetic/rt.R
@@ -1,6 +1,7 @@
 library(EpiNow2)
 library(data.table)
 library(ggplot2)
+source(here::here("inst", "dev", "recover-synthetic", "plot.R"))
 
 old_opts <- options()
 options(mc.cores = 4)
@@ -14,6 +15,8 @@ reporting_delay <- list(
   sd = convert_to_logsd(2, 1), sd_sd = 0.1, max = 15
 )
 
+obs <- obs_opts(scale = list(mean = 0.1, sd = 0.025), return_likelihood = TRUE)
+
 # fit model to data to recover realistic parameter estimates and define settings
 # shared simulation settings
 init <- estimate_infections(example_confirmed[1:100],
@@ -21,19 +24,17 @@ init <- estimate_infections(example_confirmed[1:100],
   delays = delay_opts(incubation_period, reporting_delay),
   rt = rt_opts(prior = list(mean = 2, sd = 0.1), rw = 14),
   gp = NULL, horizon = 0,
-  obs = obs_opts(scale = list(mean = 0.1, sd = 0.025))
+  obs = obs
 )
-
 
 # Rt scenario
 R <- c(
   rep(2, 40), rep(0.5, 10), rep(1, 10), 1 + 0.04 * 1:20, rep(1.4, 5),
   1.4 - 0.02 * 1:20, rep(1.4, 10), rep(0.8, 5), 0.8 + 0.02 * 1:20
 )
-noisy_R <- R * rnorm(140, 1, 0.05)
+noisy_R <- R * rnorm(length(R), 1, 0.05)
 # update Rt trajectory and simulate new infections using it
 sims <- simulate_infections(init, R = noisy_R, samples = 10)
-plot(sims)
 
 sim_R <- sims$summarised[variable == "R"]$median
 
@@ -42,31 +43,7 @@ posterior_sample <- sims$samples[sample == 1]
 sim_cases <- posterior_sample[variable == "reported_cases", .(date, confirm = value)]
 sim_inf <- posterior_sample[variable == "infections"]$value
 
-# plot Rt vs data
-plot_rt <- function(est, R) {
-  plot_estimates(
-    estimate = est$summarised[variable == "R"],
-    reported = data.table::data.table(
-      date = sims$summarised[variable == "R"]$date,
-      confirm = R
-    ),
-    obs_as_col = FALSE,
-    ylab = "Effective Reproduction No.",
-    hline = 1
-  )
-}
-
-save_ggplot <- function(plot, name) {
-  ggplot2::ggsave(paste0("inst/dev/figs/rt_", name, ".png"),
-    plot,
-    dpi = 300, width = 9, height = 6
-  )
-}
-
-make_plot <- function(est, R, name) {
-  plot <- plot_rt(est, R)
-  save_ggplot(plot, name)
-}
+save_ggplot(plot(sims), "sims")
 
 gp <- list()
 backcalc <- list()
@@ -90,11 +67,13 @@ for (method in c("nuts")) {
       delays = delay_opts(incubation_period, reporting_delay),
       rt = rt_opts(prior = list(mean = 2, sd = 0.25)),
       stan = stanopts,
-      obs = obs_opts(scale = list(mean = 0.1, sd = 0.025)),
+      obs = obs,
       horizon = 0
     )
   # runtime ~ 10 minutes
-  make_plot(gp[[method]], R, paste("gp", method, sep = "_"))
+  make_plots(
+   gp[[method]], R, sim_inf, paste("gp", method, sep = "_")
+  )
 
   # Backcalculation
   backcalc[[method]] <-
@@ -103,10 +82,13 @@ for (method in c("nuts")) {
       delays = delay_opts(incubation_period, reporting_delay),
       rt = NULL,
       stan = stanopts,
+      obs = obs,
       horizon = 0
     )
   # runtime ~ 15 seconds
-  make_plot(backcalc[[method]], R, paste("backcalc", method, sep = "_"))
+  make_plots(
+    backcalc[[method]], R, sim_inf, paste("backcalc", method, sep = "_")
+  )
 
   # RW (weekly)
   weekly_rw[[method]] <-
@@ -119,11 +101,13 @@ for (method in c("nuts")) {
       ),
       gp = NULL,
       stan = stanopts,
-      obs = obs_opts(scale = list(mean = 0.1, sd = 0.025)),
+      obs = obs,
       horizon = 0
     )
   # runtime ~ 5 minutes
-  make_plot(weekly_rw[[method]], R, paste("weekly_rw", method, sep = "_"))
+  make_plots(
+    weekly_rw[[method]], R, sim_inf, paste("weekly_rw", method, sep = "_")
+  )
 
   # RW (every month) + stationary Guassian process
   gp_rw[[method]] <-
@@ -134,12 +118,14 @@ for (method in c("nuts")) {
         prior = list(mean = 2, sd = 0.25), rw = 14, gp_on = "R0"
       ),
       stan = stanopts,
-      obs = obs_opts(scale = list(mean = 0.1, sd = 0.025)),
+      obs = obs,
       horizon = 0
     )
 
   # runtime ~ 10 minutes
-  make_plot(gp_rw[[method]], R, paste("gp_rw", method, sep = "_"))
+  make_plots(
+    gp_rw[[method]], R, sim_inf, paste("gp_rw", method, sep = "_")
+  )
 
   # Daily RW
   if (fit_daily) {
@@ -153,11 +139,13 @@ for (method in c("nuts")) {
         ),
         gp = NULL,
         stan = stanopts,
-        obs = obs_opts(scale = list(mean = 0.1, sd = 0.025)),
+        obs = obs,
         horizon = 0
       )
     # runtime ~ 10 minutes (with 40+ divergent transitions)
-    make_plot(daily_rw[[method]], R, paste("daily_rw", method, sep = "_"))
+    make_plots(
+      daily_rw[[method]], R, sim_inf, paste("daily_rw", method, sep = "_")
+    )
   }
 }
 
@@ -185,102 +173,5 @@ saveRDS(list(
 ),
 file = "synthetic.rds",
 )
-
-# library(dplyr)
-# library(tidyr)
-# library(rstan)
-# library(loo)
-# library(matrixStats)
-
-# looic <- lapply(models, function(x) loo(x$fit, save_psis = TRUE))
-# looic <- lapply(looic, function(x) x$estimates["looic", ])
-
-# dl <- bind_rows(looic) %>%
-#   mutate(
-#     model = model_names,
-#     model = sub("^(.*)_([^_]+)$", "\\1|\\2", model)
-#   ) %>%
-#   separate(model, c("model", "method"), sep = "\\|") %>%
-#   arrange(Estimate)
-
-# R_samples <- lapply(models, function(x) {
-#   if ("R[1]" %in% names(x$fit)) {
-#     rstan::extract(x$fit, "R")$R
-#   } else {
-#     rstan::extract(x$fit, "gen_R")$gen_R
-#   }
-# })
-# inf_samples <- lapply(models, function(x) rstan::extract(x$fit, "infections")$infections)
-
-# rcrps <- lapply(R_samples, function(x) if (!is.null(x)) scoringutils::crps(R, t(x)))
-# names(rcrps) <- model_names
-# icrps <- lapply(inf_samples, function(x) if (!is.null(x)) scoringutils::crps(sim_inf, t(x)))
-# names(icrps) <- model_names
-
-# rmeanae <- lapply(R_samples, function(x) if (!is.null(x)) abs(R - colMeans(x)))
-# names(rmeanae) <- model_names
-# imeanae <- lapply(inf_samples, function(x) if (!is.null(x)) abs(sim_inf - colMeans(x)))
-# names(imeanae) <- model_names
-
-# rmedianae <- lapply(R_samples, function(x) if (!is.null(x)) abs(R - colMedians(x)))
-# names(rmedianae) <- model_names
-# imedianae <- lapply(inf_samples, function(x) if (!is.null(x)) abs(sim_inf - colMedians(x)))
-# names(imedianae) <- model_names
-
-# rdf <- bind_rows(rcrps) %>%
-#   mutate(metric = "CRPS") %>%
-#   bind_rows(rmeanae) %>%
-#   mutate(metric = if_else(is.na(metric), "AE of mean", metric)) %>%
-#   bind_rows(rmedianae) %>%
-#   mutate(metric = if_else(is.na(metric), "AE of median", metric)) %>%
-#   mutate(time = rep(1:(n() / 3), times = 3)) %>%
-#   pivot_longer(names_to = "model", c(-time, -metric))
-
-# p <- ggplot(rdf, aes(x = time, y = value, colour = model)) +
-#   geom_line() +
-#   scale_colour_brewer("Model", palette = "Dark2") +
-#   facet_wrap(~metric, ncol = 1, scale = "free_y") +
-#   xlab("Time") +
-#   ylab("Score") +
-#   ggplot2::theme_bw() +
-#   ggtitle("Reconstructing R")
-
-# save_ggplot(p, "rscores")
-
-# rdf %>%
-#   select(-time) %>%
-#   group_by(metric, model) %>%
-#   summarise_all(mean) %>%
-#   pivot_wider(names_from = "metric") %>%
-#   arrange(CRPS)
-
-# idf <- bind_rows(icrps) %>%
-#   mutate(metric = "CRPS") %>%
-#   bind_rows(imeanae) %>%
-#   mutate(metric = if_else(is.na(metric), "AE of mean", metric)) %>%
-#   bind_rows(imedianae) %>%
-#   mutate(metric = if_else(is.na(metric), "AE of median", metric)) %>%
-#   mutate(time = rep(1:(n() / 3), times = 3)) %>%
-#   pivot_longer(names_to = "model", c(-time, -metric)) %>%
-#   mutate(time = time - 7) %>%
-#   filter(time >= 0)
-
-# p <- ggplot(idf, aes(x = time, y = value, colour = model)) +
-#   geom_line() +
-#   scale_colour_brewer("Model", palette = "Dark2") +
-#   facet_wrap(~metric, ncol = 1, scale = "free_y") +
-#   xlab("Time") +
-#   ylab("Score") +
-#   ggplot2::theme_bw() +
-#   ggtitle("Reconstructing infections")
-
-# save_ggplot(p, "iscores")
-
-# idf %>%
-#   select(-time) %>%
-#   group_by(metric, model) %>%
-#   summarise_all(mean) %>%
-#   pivot_wider(names_from = "metric") %>%
-#   arrange(CRPS)
 
 options(old_opts)
