e13a4c503fdb506a85d2ecac91c39302c7cba55a
Sam <s.e.abbott12@gmail.com>
2023-06-12 09:46:51 +0100

    Hotfix: Make touchstone uncertain test work as expected

--- touchstone/setup.R
+++ touchstone/setup.R
@@ -12,9 +12,27 @@ incubation_period <- get_incubation_period(
   disease = "SARS-CoV-2", source = "lauer", fixed = TRUE
 )
 
-reporting_delay <- list(
+reporting_delay <- dist_spec(
   mean = convert_to_logmean(2, 1), mean_sd = 0,
   sd = convert_to_logsd(2, 1), sd_sd = 0, max = 10
 )
 
-delays <- delay_opts(incubation_period, reporting_delay)
+delays <- delay_opts(incubation_period + reporting_delay)
+
+
+# set up example generation time
+u_generation_time <- get_generation_time(
+  disease = "SARS-CoV-2", source = "ganyani", fixed = FALSE
+)
+
+# set delays between infection and case report
+u_incubation_period <- get_incubation_period(
+  disease = "SARS-CoV-2", source = "lauer", fixed = FALSE
+)
+
+u_reporting_delay <- dist_spec(
+  mean = convert_to_logmean(2, 1), mean_sd = 0.1,
+  sd = convert_to_logsd(2, 1), sd_sd = 0.1, max = 10
+)
+
+u_delays <- delay_opts(u_incubation_period + u_reporting_delay)
