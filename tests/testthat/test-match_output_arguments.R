ff6307872837cc4a8c711d21313d874ad4422966
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-18 11:18:54 +0100

    qualify internal functions

--- tests/testthat/test-match_output_arguments.R
+++ tests/testthat/test-match_output_arguments.R
@@ -2,14 +2,18 @@ test_that("match_output_arguments works as expected", {
   out <- rep(FALSE, 3)
   names(out) <- c("fit", "plots", "samples")
   expect_equal(
-    match_output_arguments(supported_args = names(out)),
+    EpiNow2:::match_output_arguments(supported_args = names(out)),
     out
   )
   out["plots"] <- TRUE
-  expect_equal(match_output_arguments("plots", supported_args = names(out)), out)
+  expect_equal(
+    EpiNow2:::match_output_arguments("plots", supported_args = names(out)), out
+  )
   out["samples"] <- TRUE
-  expect_equal(match_output_arguments(c("plots", "samples"),
+  expect_equal(EpiNow2:::match_output_arguments(c("plots", "samples"),
     supported_args = names(out)
   ), out)
-  expect_equal(match_output_arguments("p", supported_args = names(out)), out)
+  expect_equal(
+    EpiNow2:::match_output_arguments("p", supported_args = names(out)), out
+  )
 })
