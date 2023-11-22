b70c06f64cb5dfa05982f2b97a287167e1482431
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-11-22 09:35:55 +0000

    use uncertain reporting in example

--- data-raw/reporting-delay.R
+++ data-raw/reporting-delay.R
@@ -2,6 +2,8 @@ library(EpiNow2)
 
 ## example reporting delay
 
-example_reporting_delay <- lognormal(mean = 2, sd = 1, max = 10L)
+example_reporting_delay <- lognormal(
+  meanlog = normal(0.6, 0.06), sdlog = normal(0.5, 0.05), max = 10L
+)
 
 usethis::use_data(example_reporting_delay, overwrite = TRUE)
