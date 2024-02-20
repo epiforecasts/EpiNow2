8e2e2d83a17e90b89151d793123c62721248d75e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-02-20 12:41:26 +0000

    add deprecation context and re-render doc

--- R/simulate_infections.R
+++ R/simulate_infections.R
@@ -9,10 +9,12 @@
 #' Uncertain parameters are not allowed.
 #'
 #' A previous function called [simulate_infections()] that simulates from a
-#' given model fit has been renamed [forecast_infections()].
+#' given model fit has been renamed [forecast_infections()]. Using
+#' [simulate_infections()] with existing estimates is now deprecated. This
+#' option will be removed in version 2.1.0.
 #' @param R a data frame of reproduction numbers (column `R`) by date (column
-#'   `date`). Column `R` must be numeric and `date` must be in date format. If any dates
-#'   are missing, it will be assumed that R stays the same.
+#'   `date`). Column `R` must be numeric and `date` must be in date format. If
+#'   any dates are missing, it will be assumed that R stays the same.
 #' @param initial_infections numeric; the initial number of infections.
 #' @param day_of_week_effect either `NULL` (no day of the week effect) or a
 #'   numerical vector of length specified in [obs_opts()] as `week_length`
