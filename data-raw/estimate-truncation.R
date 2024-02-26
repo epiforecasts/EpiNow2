56ad74648562a48e83f52e1d68efee2b0e4c34b1
James Azam <james.m.azam@gmail.com>
2024-02-26 17:49:47 +0000

    Add script to create data for estimate truncation example

---
+++ data-raw/estimate-truncation.R
@@ -0,0 +1,29 @@
+library("EpiNow2")
+library("here")
+
+# get example case counts
+reported_cases <- example_confirmed[1:60]
+
+# define example truncation distribution (note not integer adjusted)
+trunc_dist <- dist_spec(
+  mean = convert_to_logmean(3, 2),
+  mean_sd = 0.1,
+  sd = convert_to_logsd(3, 2),
+  sd_sd = 0.1,
+  max = 10
+)
+
+# Use the make_truncated_data function to generate example data for
+# an example using estimate_truncation()
+example_truncation_data <- purrr::map(
+  c(20, 15, 10, 0),
+  make_truncated_data,
+  data = reported_cases,
+  dist = trunc_dist
+)
+
+saveRDS(
+  example_truncation_data,
+  here("inst", "extdata", "example_truncation_data.rds"),
+  compress = "xz"
+)
