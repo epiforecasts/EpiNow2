0f3a6d543df9cfcd833307a341a9cc09021259a7
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-02-29 13:23:42 +0000

    update stan secondary tests

--- tests/testthat/test-stan-secondary.R
+++ tests/testthat/test-stan-secondary.R
@@ -4,7 +4,9 @@ skip_on_os("windows")
 # test primary reports and observations
 reports <- rep(10, 20)
 obs <- rep(4, 20)
-delay_pmf <- reverse_mf(discretised_pmf(log(3), 0.1, 5, 0))
+delay_rev_pmf <- reverse_mf(discretised_pmf(log(3), 0.1, 5, 0))
+scaled <- reports * 0.1
+convolved <- rep(1e-5, 20) + convolve_to_report(scaled, delay_rev_pmf, 0)
 
 check_equal <- function(args, target, dof = 0, dev = FALSE) {
   out <- do.call(calculate_secondary, args)
@@ -17,46 +19,46 @@ check_equal <- function(args, target, dof = 0, dev = FALSE) {
 
 test_that("calculate_secondary can calculate prevalence as expected", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 1, 1, 1, 1, 1, 20),
+    args = list(scaled, convolved, obs, 1, 1, 1, 1, 1, 20),
     target = c(1, 5, 5.5, rep(6, 17)), dof = 1
   )
 })
 
 test_that("calculate_secondary can calculate incidence as expected", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 0, 1, 1, 1, 1, 20),
+    args = list(scaled, convolved, obs, 0, 1, 1, 1, 1, 20),
     target = c(1, 1, 1.5, rep(2.0, 17)), dof = 1
   )
 })
 
 test_that("calculate_secondary can calculate incidence as expected", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 0, 1, 1, 1, 1, 20),
+    args = list(scaled, convolved, obs, 0, 1, 1, 1, 1, 20),
     target = c(1, 1, 1.5, rep(2.0, 17)), dof = 1
   )
 })
 
 test_that("calculate_secondary can calculate incidence using only historic reports", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 0, 1, 1, 0, 1, 20),
+    args = list(scaled, convolved, obs, 0, 1, 1, 0, 1, 20),
     target = c(0, 0, rep(1, 18)), dof = 0
   )
 })
 
 test_that("calculate_secondary can calculate incidence using only current reports", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 0, 0, 1, 1, 1, 20),
+    args = list(scaled, convolved, obs, 0, 0, 1, 1, 1, 20),
     target = rep(1, 20), dof = 0
   )
 })
 
 test_that("calculate_secondary can switch into prediction mode as expected", {
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 1, 0, 1, 1, 1, 20),
+    args = list(scaled, convolved, obs, 1, 0, 1, 1, 1, 20),
     target = c(1, rep(5, 19)), dof = 0
   )
   check_equal(
-    args = list(reports, obs, 0.1, delay_pmf, 1, 0, 1, 1, 1, 10),
+    args = list(scaled, convolved, obs, 1, 0, 1, 1, 1, 10),
     target = c(1, rep(5, 9), 6:15), dof = 0
   )
 })
