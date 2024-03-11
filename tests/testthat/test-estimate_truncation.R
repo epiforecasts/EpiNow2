e05a05ab1560c965df8bdada493471dd6344dcda
jamesaazam <james.azam@lshtm.ac.uk>
2024-03-11 17:40:06 +0000

    Add tests for the new arguments

--- tests/testthat/test-estimate_truncation.R
+++ tests/testthat/test-estimate_truncation.R
@@ -37,6 +37,36 @@ test_that("estimate_truncation can return values from simulated data with the
   expect_error(plot(est), NA)
 })
 
+test_that("estimate_truncation works with filter_leading_zeros set", {
+  skip_on_os("windows")
+  # fit model to a modified version of example_data with zero leading cases
+  # but with filter_leading_zeros = TRUE
+  modified_data <- example_truncated
+  modified_data <- purrr::map(modified_data, \(x) x[1:3, confirm := 0])
+  out <- estimate_truncation(modified_data,
+                             verbose = FALSE, chains = 2, iter = 1000, warmup = 250,
+                             stan = stan_opts(backend = "cmdstanr"),
+                             filter_leading_zeros = TRUE
+  )
+  expect_named(out, c("dist", "obs", "last_obs", "cmf", "data", "fit"))
+  expect_s3_class(est$dist, "dist_spec")
+})
+
+test_that("estimate_truncation works with zero_threshold set", {
+  skip_on_os("windows")
+  # fit model to a modified version of example_data with zero leading cases
+  # but with filter_leading_zeros = TRUE
+  modified_data <- example_truncated
+  modified_data <- purrr::map(modified_data, \(x) x[sample(1:10, 6), confirm := 0])
+  out <- estimate_truncation(modified_data,
+                             verbose = FALSE, chains = 2, iter = 1000, warmup = 250,
+                             stan = stan_opts(backend = "cmdstanr"),
+                             zero_threshold = 1
+  )
+  expect_named(out, c("dist", "obs", "last_obs", "cmf", "data", "fit"))
+  expect_s3_class(est$dist, "dist_spec")
+})
+
 test_that("deprecated arguments are recognised", {
   options(warn = 2)
   expect_error(estimate_truncation(example_truncated,
