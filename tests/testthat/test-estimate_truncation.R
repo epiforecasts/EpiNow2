c11f3e56e8051e13fe317d5608a01faf8ce70c24
James Azam <james.m.azam@gmail.com>
2024-03-26 16:52:01 +0000

    Improve test to compare aspects of fits to original dataset

--- tests/testthat/test-estimate_truncation.R
+++ tests/testthat/test-estimate_truncation.R
@@ -39,17 +39,37 @@ test_that("estimate_truncation can return values from simulated data with the
 
 test_that("estimate_truncation works with filter_leading_zeros set", {
   skip_on_os("windows")
-  # fit model to a modified version of example_data with zero leading cases
-  # but with filter_leading_zeros = TRUE
-  modified_data <- example_truncated
-  modified_data <- purrr::map(modified_data, function(x) x[1:3, confirm := 0])
-  out <- estimate_truncation(modified_data,
-                             verbose = FALSE, chains = 2, iter = 1000, warmup = 250,
-                             stan = stan_opts(backend = "cmdstanr"),
-                             filter_leading_zeros = TRUE
+  # Modify the first three rows of the first dataset to have zero cases
+  # and fit the model with filter_leading_zeros = TRUE. This should
+  # be the same as fitting the model to the original dataset because the
+  # earlier dataset is corrected to be the same as the final dataset.
+  modified_data <- data.table::copy(example_truncated)
+  modified_data[[1]][1:3, confirm := 0]
+  modified_data_fit <- estimate_truncation(
+    modified_data,
+    verbose = FALSE, chains = 2, iter = 1000, warmup = 250,
+    stan = stan_opts(backend = "cmdstanr"),
+    filter_leading_zeros = TRUE
   )
-  expect_named(out, c("dist", "obs", "last_obs", "cmf", "data", "fit"))
-  expect_s3_class(out$dist, "dist_spec")
+  # fit model to original dataset
+  original_data_fit <- estimate_truncation(
+    example_truncated,
+    verbose = FALSE, chains = 2, iter = 1000, warmup = 250,
+    stan = stan_opts(backend = "cmdstanr")
+  )
+  expect_named(
+    modified_data_fit,
+    c("dist", "obs", "last_obs", "cmf", "data", "fit")
+  )
+  # Compare the results of the two fits
+  expect_equal(
+    original_data_fit$dist$dist,
+    modified_data_fit$dist$dist
+  )
+  expect_equal(
+    original_data_fit$data$obs_dist,
+    modified_data_fit$data$obs_dist
+  )
 })
 
 test_that("estimate_truncation works with zero_threshold set", {
