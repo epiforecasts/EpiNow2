e296a8984b047ad299a2e3b7673fd3b6995b11df
Sam <s.e.abbott12@gmail.com>
2024-08-12 16:54:17 +0100

    fix tests for create_gp_data

--- tests/testthat/test-create_gp_data.R
+++ tests/testthat/test-create_gp_data.R
@@ -1,14 +1,14 @@
 test_that("create_gp_data returns correct default values when GP is disabled", {
   data <- list(t = 30, seeding_time = 7, horizon = 7, future_fixed = 0, fixed_from = 0)
   gp_data <- create_gp_data(NULL, data)
-  expect_true(gp_data$fixed)
+  expect_equal(gp_data$fixed, 1)
   expect_equal(gp_data$stationary, 1)
-  expect_equal(gp_data$M, 18)  # (30 - 7) * 0.2
+  expect_equal(gp_data$M, 5)  # (30 - 7) * 0.2
   expect_equal(gp_data$L, 1.5)
   expect_equal(gp_data$ls_meanlog, convert_to_logmean(21, 7))
   expect_equal(gp_data$ls_sdlog, convert_to_logsd(21, 7))
   expect_equal(gp_data$ls_min, 0)
-  expect_equal(gp_data$ls_max, 16)  # 30 - 7 - 7
+  expect_equal(gp_data$ls_max, 16)
   expect_equal(gp_data$alpha_sd, 0.05)
   expect_equal(gp_data$gp_type, 2)  # Default to Matern
   expect_equal(gp_data$nu, 3 / 2)
@@ -45,5 +45,5 @@ test_that("create_gp_data correctly adjusts ls_max", {
 test_that("create_gp_data correctly handles future_fixed", {
   data <- list(t = 30, seeding_time = 7, horizon = 7, future_fixed = 1, fixed_from = 2)
   gp_data <- create_gp_data(gp_opts(), data)
-  expect_equal(gp_data$M, 16)  # (30 - 7 - (7 - 2)) * 0.2
+  expect_equal(gp_data$M, 4) 
 })
