b5525ffe1c9ace16a9d0a4f653502d85f4d14f14
jamesaazam <james.azam@lshtm.ac.uk>
2024-09-17 17:46:08 +0100

    Add new global

--- R/utilities.R
+++ R/utilities.R
@@ -441,6 +441,7 @@ globalVariables(
     "central_lower", "central_upper", "mean_sd", "sd_sd", "average_7_day",
     "..lowers", "..upper_CrI", "..uppers", "timing", "dataset", "last_confirm",
     "report_date", "secondary", "id", "conv", "meanlog", "primary", "scaled",
-    "scaling", "sdlog", "lookup", "new_draw", ".draw", "p", "distribution"
+    "scaling", "sdlog", "lookup", "new_draw", ".draw", "p", "distribution",
+    "columns_to_check"
   )
 )
