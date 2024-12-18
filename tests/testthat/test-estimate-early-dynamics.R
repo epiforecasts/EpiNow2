25bc7ced4c7bf10971cf9c32c18f4e4ce0030eaf
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-12-18 14:54:53 +0000

    update tests

--- tests/testthat/test-estimate-early-dynamics.R
+++ tests/testthat/test-estimate-early-dynamics.R
@@ -10,7 +10,7 @@ test_that("estimate_early_dynamics works", {
   # Check values
   expect_identical(
     round(prior_estimates$prior_infections, 2),
-    4.53
+    3.21
   )
   expect_identical(
     round(prior_estimates$prior_growth, 2),
@@ -21,9 +21,9 @@ test_that("estimate_early_dynamics works", {
 test_that("estimate_early_dynamics handles NA values correctly", {
   cases <- c(10, 20, NA, 40, 50, NA, 70)
   prior_estimates <- estimate_early_dynamics(cases, 7)
-  expect_equal(
-    prior_estimates$prior_infections,
-    log(mean(c(10, 20, 40, 50, 70), na.rm = TRUE))
+  expect_identical(
+    round(prior_estimates$prior_infections, 2),
+    2.55
   )
   expect_true(!is.na(prior_estimates$prior_growth))
 })
@@ -31,14 +31,14 @@ test_that("estimate_early_dynamics handles NA values correctly", {
 test_that("estimate_early_dynamics handles exponential growth", {
   cases <- 2^(c(0:6)) # Exponential growth
   prior_estimates <- estimate_early_dynamics(cases, 7)
-  expect_equal(prior_estimates$prior_infections, log(mean(cases[1:7])))
+  expect_equal(prior_estimates$prior_infections, log(2^0))
   expect_true(prior_estimates$prior_growth > 0) # Growth should be positive
 })
 
 test_that("estimate_early_dynamics handles exponential decline", {
   cases <- rev(2^(c(0:6))) # Exponential decline
   prior_estimates <- estimate_early_dynamics(cases, 7)
-  expect_equal(prior_estimates$prior_infections, log(mean(cases[1:7])))
+  expect_equal(prior_estimates$prior_infections, log(2^6))
   expect_true(prior_estimates$prior_growth < 0) # Growth should be negative
 })
 
