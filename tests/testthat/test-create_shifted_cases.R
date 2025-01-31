30dc937c0e1c1b3ef6544d18b1eed0c402d7caf4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-01-31 11:20:33 +0000

    specify horizon

--- tests/testthat/test-create_shifted_cases.R
+++ tests/testthat/test-create_shifted_cases.R
@@ -1,3 +1,4 @@
+horizon <- 7
 cases <- create_clean_reported_cases(example_confirmed[1:30])
 cases <- add_horizon(cases, horizon)
 ## add zeroes initially
@@ -14,7 +15,7 @@ cases <- data.table::rbindlist(list(
 ))
 
 test_that("create_shifted_cases does not create discontinuities with exponential growth", {
-  result <- create_shifted_cases(cases, 7, 14, 7)
+  result <- create_shifted_cases(cases, 7, 14, horizon)
   expect_s3_class(result, "data.table")
   expect_true(all(diff(result$confirm) > 0))
 })
