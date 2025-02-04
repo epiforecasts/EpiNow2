8484fcbcec05fae52a8e6cdce195ec5d015c9edc
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-02-04 11:56:20 +0000

    reduce size of data files

--- data-raw/estimate-infections.R
+++ data-raw/estimate-infections.R
@@ -1,5 +1,7 @@
 library("EpiNow2")
+library("EpiNow2")
 library("here")
+library("purrr")
 
 options(mc.cores = 4)
 
@@ -15,7 +17,9 @@ example_estimate_infections <- estimate_infections(reported_cases,
   generation_time = gt_opts(example_generation_time),
   delays = delay_opts(example_incubation_period + reporting_delay),
   rt = rt_opts(prior = LogNormal(mean = 2, sd = 0.1)),
-  stan = stan_opts(samples = 200, control = list(adapt_delta = 0.95))
+  stan = stan_opts(
+    samples = 200, control = list(adapt_delta = 0.95), return_fit = FALSE
+  )
 )
 
 cases <- example_confirmed[1:60]
@@ -29,7 +33,21 @@ example_regional_epinow <- regional_epinow(
   data = cases,
   delays = delay_opts(example_incubation_period + reporting_delay),
   rt = rt_opts(prior = LogNormal(mean = 2, sd = 0.2)),
-  stan = stan_opts(samples = 200, control = list(adapt_delta = 0.95))
+  stan = stan_opts(
+    samples = 200, control = list(adapt_delta = 0.95), return_fit = FALSE
+  )
+)
+
+## remove plots
+example_regional_epinow <- modify_tree(
+  example_regional_epinow,
+  post = function(x) {
+    plot_names <- grep("plots?$", names(x), value = TRUE)
+    for (name in plot_names) {
+      x[[name]] <- NULL
+    }
+    return(x)
+  }
 )
 
 saveRDS(
