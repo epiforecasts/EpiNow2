9f59e875d816247dc690960703a5e2eb6acc2a56
James Azam <james.m.azam@gmail.com>
2025-03-13 14:35:36 +0000

    Add sentence abouting prior predictive checks

--- R/estimate_infections.R
+++ R/estimate_infections.R
@@ -18,8 +18,9 @@
 #' estimate Rt for Covid-19 in a country from the ECDC data source.
 #'
 #' @param data A `<data.frame>` of disease reports (confirm) by date (date).
-#' `confirm` must be numeric and `date` must be in date format. Optionally
-#' this can also have a logical `accumulate` column which indicates whether
+#' `confirm` must be numeric and `date` must be in date format. `confirm` can
+#' also be set to a series of `NA` to do prior predictive checks. Optionally,
+#' `data` can also have a logical `accumulate` column which indicates whether
 #' data should be added to the next data point. This is useful when modelling
 #' e.g. weekly incidence data. See also the [fill_missing()] function which
 #' helps add the `accumulate` column with the desired properties when dealing
