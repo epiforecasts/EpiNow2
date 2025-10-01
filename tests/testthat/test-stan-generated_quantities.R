2259ea3185358f893a1821b1d8b7285ab6119c33
Adrian Lison <adrian.lison@bsse.ethz.ch>
2025-10-01 23:14:26 +0200

    Add tests for calculate growth methods

--- tests/testthat/test-stan-generated_quantities.R
+++ tests/testthat/test-stan-generated_quantities.R
@@ -13,3 +13,53 @@ test_that("calculate_growth_infections works as expected", {
   expect_error(calculate_growth_infections(1:5, 6))
   expect_error(calculate_growth_infections(1:5, 0))
 })
+
+test_that("calculate_growth_infness works as expected", {
+  skip_on_cran()
+  gt_rev_pmf <- rev(c(0.5, 0.3, 0.2))
+  mean_gt <- round(sum(gt_rev_pmf * seq_along(gt_rev_pmf)))
+  
+  expect_equal(
+    calculate_growth_infness(rep(1, 7), 1, gt_rev_pmf),
+    c(rep(0, 6 - mean_gt), rep(NaN, mean_gt))
+    )
+  
+  seeding <- 2
+  infness <- stable_convolve(1:7, gt_rev_pmf)
+  growth <- diff(log(infness))[(mean_gt+seeding):(length(infness)-1-mean_gt)]
+  expect_equal(
+    round(calculate_growth_infness(1:7, seeding, gt_rev_pmf), 2),
+    c(round(growth, 2), rep(NaN, mean_gt))
+  )
+  
+  seeding <- 3
+  infness <- stable_convolve(exp(0.4 * 1:7), gt_rev_pmf)
+  growth <- diff(log(infness))[(mean_gt+seeding):(length(infness)-1-mean_gt)]
+  expect_equal(
+    round(calculate_growth_infness(exp(0.4 * 1:7), seeding, gt_rev_pmf), 2),
+    c(round(growth, 2), rep(NaN, mean_gt))
+  )
+  
+  expect_error(calculate_growth_infness(1:5, 6, gt_rev_pmf))
+})
+
+test_that("calculate_growth selects the right method", {
+  skip_on_cran()
+  
+  gt_rev_pmf <- rev(c(0.5, 0.3, 0.2))
+  mean_gt <- round(sum(gt_rev_pmf * seq_along(gt_rev_pmf)))
+  
+  seeding <- 2
+  infness <- stable_convolve(1:7, gt_rev_pmf)
+  growth <- diff(log(infness))[(mean_gt+seeding):(length(infness)-1-mean_gt)]
+  
+  expect_equal(
+    round(calculate_growth(1:7, seeding, gt_rev_pmf, 0), 2),
+    c(0.29, 0.22, 0.18, 0.15)
+  )
+  
+  expect_equal(
+    round(calculate_growth(1:7, seeding, gt_rev_pmf, 1), 2),
+    c(round(growth, 2), rep(NaN, mean_gt))
+  )
+})
