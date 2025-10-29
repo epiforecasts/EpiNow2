acb568986b3823ab130ba0ee888b10f80f5c2c1f
Sam Abbott <contact@samabbott.co.uk>
2025-10-29 17:37:07 +0000

    Remove self-referencing plot methods from seealso tags
    Plot methods should not reference themselves in seealso tags. Removed:
    - plot.estimate_infections() from its own seealso (kept report_plots)
    - plot.estimate_secondary() from its own seealso (kept estimate_secondary)
    - plot.estimate_truncation() from its own seealso (kept estimate_truncation)

--- R/plot.R
+++ R/plot.R
@@ -377,7 +377,7 @@ plot_summary <- function(summary_results,
 #' @param ... Pass additional arguments to report_plots
 #' @importFrom rlang arg_match
 #'
-#' @seealso [plot.estimate_infections()] [report_plots()]
+#' @seealso [report_plots()]
 #' @aliases plot
 #' @method plot estimate_infections
 #' @return List of plots as produced by [report_plots()]
@@ -414,7 +414,6 @@ plot.estimate_infections <- function(x,
 #' @param x A list of output as produced by [epinow()].
 #' @inheritParams plot.estimate_infections
 #' @seealso [plot.estimate_infections()] [report_plots()]
-#' [estimate_infections()]
 #' @method plot epinow
 #' @return List of plots as produced by [report_plots()]
 #' @export
