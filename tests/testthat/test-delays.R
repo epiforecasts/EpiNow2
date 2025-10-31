256bb78305116dd77d1e74d1c542f0064fee7802
Sam Abbott <contact@samabbott.co.uk>
2025-10-31 15:29:15 +0000

    Update snapshots and test for qprimarycensored quantile changes
    Platform differences and qprimarycensored quantile behavior result in
    different random values. Updated snapshots to match CI Linux platform
    values.
    
    Also fixed test-delays.R expectation to account for extra trailing zero
    in PMF from discretisation changes.

--- tests/testthat/test-delays.R
+++ tests/testthat/test-delays.R
@@ -61,7 +61,7 @@ test_that("delay parameters can be specified in different ways", {
       )),
       params = delay_params
     ), digits = 2), n = -2),
-    c(0.03, 0.38, 0.37, 0.14, 0.05, 0.02, 0.01, 0.00, 1.00)
+    c(0.03, 0.38, 0.37, 0.14, 0.05, 0.02, 0.01, 0.00, 0.00, 1.00)
   )
   expect_equal(
     test_stan_delays(
