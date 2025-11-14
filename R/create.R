802bdbb02b4a07642b9939931f92a26530b67719
jamesaazam <james.azam@lshtm.ac.uk>
2025-11-14 13:38:26 +0000

    Revert removing backend in create_stan_args

--- R/create.R
+++ R/create.R
@@ -658,6 +658,7 @@ create_stan_args <- function(stan = stan_opts(),
   ## generate stan model
   if (is.null(stan$object)) {
     stan$object <- epinow2_stan_model(stan$backend, model)
+    stan$backend <- NULL
   }
   # cmdstanr doesn't have an init = "random" argument
   if (is.character(init) && init == "random" &&
--- tests/testthat/test-create_stan_args.R
+++ tests/testthat/test-create_stan_args.R
@@ -1,6 +1,6 @@
 test_that("create_stan_args returns the expected defaults when the exact method is used", {
   expect_equal(names(create_stan_args()), c(
-    "data", "init", "refresh", "backend", "object", "method", "chains", "save_warmup",
+    "data", "init", "refresh", "object", "method", "chains", "save_warmup",
     "seed", "future", "max_execution_time", "cores", "warmup", "control", "iter"
   ))
 })
@@ -8,7 +8,7 @@ test_that("create_stan_args returns the expected defaults when the exact method 
 test_that("create_stan_args returns the expected defaults when the approximate method is used", {
   expect_equal(names(create_stan_args(stan = stan_opts(method = "vb"))), c(
     "data", "init", "refresh",
-    "backend", "object", "method",
+    "object", "method",
     "trials", "iter", "output_samples"
   ))
 })
